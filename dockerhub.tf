8dff7ed02d31130e62d6c15e4a149a1487065b7c
Dries Verachtert <dries.verachtert@epfl.ch>
2023-04-07 16:12:55 +0200

    BBPP154-34 Add dockerhub secret ARN as a variable and a policy which gives access to the secret

--- dockerhub.tf
+++ dockerhub.tf
@@ -5,3 +5,26 @@ variable "dockerhub_credentials_arn" {
   description = "The ARN of the secret containing the credentials for dockerhub to fetch images from private dockerhub repos"
   sensitive   = true
 }
+
+resource "aws_iam_policy" "dockerhub_access" {
+  name        = "keycloak-task-policy-db"
+  description = "Policy that allows access to the db"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ssm:GetParameters",
+        "secretsmanager:GetSecretValue"
+      ],
+      "Resource": [
+        "${var.dockerhub_credentials_arn}"
+      ]
+    }
+  ]
+}
+EOF
+}
