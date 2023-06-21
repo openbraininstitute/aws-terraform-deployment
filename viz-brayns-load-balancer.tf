20739260c058555ec7f893ef7931eb6f0086e9c0
Mike Kenyon <michael.kenyon@epfl.ch>
2023-06-21 13:31:58 +0200

    [NOTICKET] Access Brayns through public load balancer

--- viz-brayns-load-balancer.tf
+++ viz-brayns-load-balancer.tf
@@ -1,3 +1,25 @@
+resource "aws_lb_listener" "sbo_brayns" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "8200"
+  protocol          = "HTTPS"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content: working"
+      status_code  = "200"
+    }
+  }
+  tags = {
+    SBO_Billing = "viz"
+  }
+  depends_on = [
+    aws_lb.alb
+  ]
+}
+
 resource "aws_lb_target_group" "viz_brayns" {
   #ts:skip=AC_AWS_0492
   name        = "viz-brayns"
@@ -5,13 +27,16 @@ resource "aws_lb_target_group" "viz_brayns" {
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = data.terraform_remote_state.common.outputs.vpc_id
+  lifecycle {
+    create_before_destroy = true
+  }
   tags = {
     SBO_Billing = "viz"
   }
 }
 
 resource "aws_lb_listener_rule" "viz_brayns_8200" {
-  listener_arn = data.terraform_remote_state.common.outputs.private_alb_listener_8200_arn
+  listener_arn = aws_lb_listener.sbo_brayns.arn
   priority     = 100
 
   action {
@@ -27,6 +52,10 @@ resource "aws_lb_listener_rule" "viz_brayns_8200" {
   tags = {
     SBO_Billing = "viz"
   }
+  depends_on = [
+    aws_lb_listener.sbo_brayns,
+    aws_lb.alb
+  ]
 }
 
 resource "aws_route53_record" "private_viz_brayns" {
