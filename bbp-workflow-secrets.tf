d07cc9a1ec19f445cf183d7655150d6882c9f14a
Daniel Fernandez <daniel.fernandez@epfl.ch>
2023-08-31 11:24:13 +0200

    [BBPP154-101] Add write/read policy for Workflow secrets

--- bbp-workflow-secrets.tf
+++ bbp-workflow-secrets.tf
@@ -31,3 +31,41 @@ EOF
     SBO_Billing = "workflow"
   }
 }
+
+resource "aws_ssoadmin_permission_set" "write_read_access_workflow_secrets" {
+  name             = "WriteReadAccessWorkflowSecrets"
+  description      = "Write and Read access for Workflow secrets"
+  instance_arn     = var.aws_iam_identity_center_arn
+  session_duration = "PT2H"
+  tags = {
+    SBO_Billing = "common"
+  }
+}
+
+resource "aws_ssoadmin_permission_set_inline_policy" "write_read_access_workflow_secrets_inline_policy" {
+  inline_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      jsondecode(local.write_read_access_workflow_secrets_policy),
+      jsondecode(local.readonly_access_policy_statement_part1),
+      jsondecode(local.readonly_access_policy_statement_part2),
+    ]
+  })
+  instance_arn       = var.aws_iam_identity_center_arn
+  permission_set_arn = aws_ssoadmin_permission_set.write_read_access_workflow_secrets.arn
+}
+
+locals {
+  write_read_access_workflow_secrets_policy = jsonencode({
+    Effect = "Allow"
+    Action = [
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:CreateSecret",
+      "secretsmanager:UpdateSecret"
+    ],
+    "Resource" : [
+      "${var.bbp_workflow_secrets_arn}"
+    ]
+  })
+}
