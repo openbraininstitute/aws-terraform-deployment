73075b227a9784ffd9581c4b5b5dc99c7195b43a
Mike Gevaert <michael.gevaert@epfl.ch>
2024-01-22 09:47:56 +0100

    NOTICKET: add mgevaert test key for debugging

--- cells_ec2_ecs_user_data.sh
+++ cells_ec2_ecs_user_data.sh
@@ -2,6 +2,9 @@
 
 echo ECS_CLUSTER='${ecs_cluster_name}' >> /etc/ecs/ecs.config
 
+#TODO: remove this key; currently for debugging
+echo -e "\nssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBCRTRSpJMLRRk0GuIcQ/OU5fGwgX0YhIMsy/sSgdzQc gevaert@bbd-jp8lt73\n" >> /home/ec2-user/.ssh/authorized_keys
+
 wget https://s3.amazonaws.com/mountpoint-s3-release/latest/x86_64/mount-s3.rpm
 yum install -y ./mount-s3.rpm
 
