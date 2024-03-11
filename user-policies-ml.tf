c87e37e3f26402906386a6efdb9c068e17f9dfc0
Neven Gutic <neven.gutic@epfl.ch>
2024-03-11 15:08:12 +0100

    [HELP-17628] Get read/write access to MWAA instance for ML team

--- user-policies-ml.tf
+++ user-policies-ml.tf
@@ -5,6 +5,13 @@ variable "ml_secrets_arn" {
   sensitive   = true
 }
 
+variable "ml_mwaa_arn" {
+  default     = "arn:aws:airflow:us-east-1:671250183987:environment/ml-airflow"
+  type        = string
+  description = "The ARN of the Machine Learning MWAA object"
+  sensitive   = true
+}
+
 resource "aws_ssoadmin_permission_set" "write_read_access_ml_secrets" {
   name             = "WriteReadMlSecrets"
   description      = "Write and Read access for ML secrets"
@@ -40,6 +47,57 @@ locals {
     ],
     "Resource" : [
       "${var.ml_secrets_arn}"
+    ]
+  })
+}
+
+resource "aws_ssoadmin_permission_set" "write_read_access_ml_mwaa" {
+  name             = "MWAAfullAccessML"
+  description      = "Write and Read access for ML MWAA"
+  instance_arn     = var.aws_iam_identity_center_arn
+  session_duration = "PT2H"
+  tags = {
+    SBO_Billing = "common"
+  }
+}
+
+resource "aws_ssoadmin_permission_set_inline_policy" "write_read_access_ml_mwaa_inline_policy" {
+  inline_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      jsondecode(local.ml_mwaa_list_env_policy),
+      jsondecode(local.ml_mwaa_full_access_policy),
+      jsondecode(local.ml_mwaa_web_access_policy),
+    ]
+  })
+  instance_arn       = var.aws_iam_identity_center_arn
+  permission_set_arn = aws_ssoadmin_permission_set.write_read_access_ml_mwaa.arn
+}
+
+locals {
+  ml_mwaa_web_access_policy = jsonencode({
+    Effect = "Allow"
+    Action = [
+      "airflow:CreateWebLoginToken",
+    ]
+    Resource = "*"
+  })
+
+  ml_mwaa_list_env_policy = jsonencode({
+    Effect = "Allow"
+    Action = [
+      "airflow:ListEnvironments",
+    ]
+    Resource = "*"
+  })
+
+  ml_mwaa_full_access_policy = jsonencode({
+    Effect = "Allow"
+    Action = [
+      "airflow:*",
+    ]
+    "Resource" : [
+      "${var.ml_mwaa_arn}"
     ]
   })
 }
