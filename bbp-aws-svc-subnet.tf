cbcae94343de8b0ad62f78b0629c560bfa41e7fb
genrich <genrich.ivaska@epfl.ch>
2024-03-15 13:55:21 +0100

    [BBPP154-137] fix on_demand_svc ecs task start

--- bbp-aws-svc-subnet.tf
+++ bbp-aws-svc-subnet.tf
@@ -20,6 +20,13 @@ resource "aws_subnet" "bbp_aws_svc_ecs" {
   }
 }
 
+# ECS agent on EC2 probably needs routing to NAT to be able to start ECS tasks
+# could be external docker image, not clear what exactly fails without this
+resource "aws_route_table_association" "bbp_aws_svc_ec2" {
+  subnet_id      = aws_subnet.bbp_aws_svc_ec2.id
+  route_table_id = data.terraform_remote_state.common.outputs.route_table_private_subnets_id
+}
+
 # Needs access to public apigateway
 # https://repost.aws/knowledge-center/api-gateway-vpc-connections:
 # Important: Resources in your VPC that try to connect to your public APIs must have internet connectivity.
