1a498fa7589ee7a316ab43882f6f6b970238cf91
Oliver Andrzej Grabinski <oliver.grabinski@epfl.ch>
2024-04-24 15:06:16 +0000

    NOTICKET Update delta to m9 and fix ship config

--- nexus/delta/variables.tf
+++ nexus/delta/variables.tf
@@ -9,7 +9,7 @@ variable "nexus_delta_hostname" {
 
 variable "nexus_delta_docker_image_url" {
   type    = string
-  default = "bluebrain/nexus-delta:1.10.0-M6"
+  default = "bluebrain/nexus-delta:1.10.0-M9"
 }
 
 variable "subnet_id" {
