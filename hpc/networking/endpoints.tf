03a5cb9b505a77c6fe588cb770ebcbe4eafd8231
Erik Heeren <erik.heeren@epfl.ch>
2024-05-06 11:54:02 +0200

    NOTICKET process review remarks

--- hpc/networking/endpoints.tf
+++ hpc/networking/endpoints.tf
@@ -1,15 +1,17 @@
 # Create VPC Endpoints for private access to CloudWatch, CloudFormation, EC2, S3, and DynamoDB
 # According to https://repost.aws/knowledge-center/ec2-systems-manager-vpc-endpoints
-# not every subnet neets an endpoint, the endpoint just needs to be in the same AZ as the subnet
+# not every subnet neets an endpoint, the endpoint just needs to be in the same AZ as the subnet.
+# In fact, it is an error to create an endpoint more than once in the same availability zone.
+# We now have dedicated subnets for the endpoints (accessible from the compute subnets), one per
+# availability zone.
 
 resource "aws_vpc_endpoint" "cloudwatch" {
   vpc_id              = var.pcluster_vpc_id
   service_name        = "com.amazonaws.us-east-1.monitoring"
   vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
-  # all of the endpoint_subnet ids - they're guaranteed to be in different availability zones, no need to get fancy
-  subnet_ids         = local.aws_subnet_compute_endpoints_ids
-  security_group_ids = var.security_groups
+  subnet_ids          = local.aws_subnet_compute_endpoints_ids
+  security_group_ids  = var.security_groups
   tags = {
     Name = "cloudwatch endpoint"
   }
--- hpc/networking/nat.tf
+++ hpc/networking/nat.tf
@@ -4,19 +4,19 @@
 #
 resource "aws_eip" "compute_eip" {
   domain = "vpc"
-  count  = var.create_compute_instances && var.compute_nat_access ? 1 : 0
+  count  = var.compute_nat_access ? 1 : 0
 }
 
 resource "aws_nat_gateway" "compute_gw" {
   allocation_id     = one(aws_eip.compute_eip[*].id)
-  count             = var.create_compute_instances && var.compute_nat_access ? 1 : 0
+  count             = var.compute_nat_access ? 1 : 0
   connectivity_type = "public"
   subnet_id         = local.aws_subnet_public_id
 }
 
 resource "aws_route" "compute_nat" {
   route_table_id         = aws_route_table.compute[count.index].id
-  count                  = var.create_compute_instances && var.compute_nat_access ? 1 : 0
+  count                  = var.compute_nat_access ? 1 : 0
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = one(aws_nat_gateway.compute_gw[*].id)
 }
