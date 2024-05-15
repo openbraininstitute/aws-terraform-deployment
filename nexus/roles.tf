b6ad543f47cab38391eb87f82c77331102846d73
Oliver Andrzej Grabinski <oliver.grabinski@epfl.ch>
2024-05-15 07:47:55 +0000

    NOTICKET new policy to write logs in cloudwatch (for nexus)

--- nexus/roles.tf
+++ nexus/roles.tf
@@ -22,6 +22,32 @@ EOF
   }
 }
 
+#tfsec:ignore:aws-iam-no-policy-wildcards
+resource "aws_iam_policy" "cloudwatch_write_policy" {
+  name        = "NexusCloudwatchWritePolicy"
+  description = "A policy that grants write access to Cloudwatch logs"
+
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
+            "logs:DescribeLogStreams"
+          ],
+          "Resource" : [
+            "arn:aws:logs:*:*:*"
+          ]
+        }
+      ]
+    }
+  )
+}
+
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_service" {
   role       = aws_iam_role.nexus_ecs_task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
@@ -35,4 +61,9 @@ resource "aws_iam_role_policy_attachment" "dockerhub_secret_access" {
 resource "aws_iam_role_policy_attachment" "nexus_secret_access" {
   role       = aws_iam_role.nexus_ecs_task_execution.name
   policy_arn = aws_iam_policy.nexus_secrets_access.arn
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_write_logs" {
+  role       = aws_iam_role.nexus_ecs_task_execution.name
+  policy_arn = aws_iam_policy.cloudwatch_write_policy.arn
 }
