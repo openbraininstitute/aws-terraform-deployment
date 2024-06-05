a18a20b84c47e6628a82e4902825ee098d424eb3
Oliver Andrzej Grabinski <oliver.grabinski@epfl.ch>
2024-06-05 11:38:35 +0000

    NOTICKET replace the old nexus db by the new

--- nexus/postgres/database-bis.tf
+++ nexus/postgres/database-bis.tf
@@ -1,40 +1,4 @@
 # tfsec:ignore:aws-rds-enable-performance-insights-encryption
-resource "aws_db_instance" "nexus" {
-  #ts:skip=AC_AWS_0053
-  #ts:skip=AC_AWS_0454
-  #ts:skip=AC_AWS_0058
-  deletion_protection     = false #tfsec:ignore:AVD-AWS-0177
-  allocated_storage       = 40    # in gigabytes
-  backup_retention_period = 2     # in days
-
-  db_subnet_group_name = aws_db_subnet_group.nexus_db_subnet_group.name
-
-  engine         = "postgres"
-  engine_version = "15"
-  multi_az       = false
-  instance_class = "db.t4g.large"
-
-  identifier = "nexus-postgres-db-old"
-  db_name    = var.nexus_postgresql_database_name
-
-  username = var.nexus_postgresql_database_username
-  #password = var.nexus_postgresql_database_password
-  password = data.aws_secretsmanager_secret_version.nexus_database_password.secret_string
-
-  publicly_accessible          = false
-  performance_insights_enabled = true
-  storage_encrypted            = false #tfsec:ignore:aws-rds-encrypt-instance-storage-data
-
-  vpc_security_group_ids = [var.subnet_security_group_id]
-
-  skip_final_snapshot                 = true
-  iam_database_authentication_enabled = false
-  tags = {
-    SBO_Billing = "nexus"
-  }
-}
-
-# tfsec:ignore:aws-rds-enable-performance-insights-encryption
 # tfsec:ignore:aws-rds-encrypt-instance-storage-data
 resource "aws_db_instance" "nexus_psql" {
   #ts:skip=AC_AWS_0053
@@ -44,8 +8,6 @@ resource "aws_db_instance" "nexus_psql" {
   allocated_storage       = 40    # in gigabytes
   backup_retention_period = 2     # in days
 
-  snapshot_identifier = "rds:nexus-postgres-db-2024-06-04-07-26"
-
   db_subnet_group_name = aws_db_subnet_group.nexus_db_subnet_group.name
 
   engine         = "postgres"
@@ -53,7 +15,7 @@ resource "aws_db_instance" "nexus_psql" {
   multi_az       = false
   instance_class = "db.t4g.medium"
 
-  identifier = "nexus-postgres-db-new"
+  identifier = "nexus-postgres-db"
   db_name    = var.nexus_postgresql_database_name
 
   username = var.nexus_postgresql_database_username
