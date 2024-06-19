f6576788e72ccaf58a2622531fae1f2aa9fd097d
Oliver Andrzej Grabinski <oliver.grabinski@epfl.ch>
2024-06-19 07:35:12 +0000

    NOTICKET set nexus db cluster subnets

--- nexus/postgres_cluster/cluster.tf
+++ nexus/postgres_cluster/cluster.tf
@@ -1,3 +1,11 @@
+resource "aws_db_subnet_group" "nexus_cluster_subnet_group" {
+  name       = "nexus-cluster-group"
+  subnet_ids = var.subnets_ids
+  tags = {
+    SBO_Billing = "nexus"
+  }
+}
+
 # TODO the secret should be defined in the code
 # Data source to retrieve the password from AWS Secrets Manager
 data "aws_secretsmanager_secret_version" "nexus_database_password" {
@@ -7,7 +15,7 @@ data "aws_secretsmanager_secret_version" "nexus_database_password" {
 # tfsec:ignore:aws-rds-encrypt-cluster-storage-data
 resource "aws_rds_cluster" "nexus" {
   cluster_identifier        = "nexus"
-  availability_zones        = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  availability_zones        = ["us-east-1a", "us-east-1b"]
   engine                    = "postgres"
   engine_version            = "15"
   db_cluster_instance_class = var.instance_class
@@ -18,6 +26,7 @@ resource "aws_rds_cluster" "nexus" {
   backup_retention_period = 7 # in days
   storage_encrypted       = false
 
+  db_subnet_group_name   = aws_db_subnet_group.nexus_cluster_subnet_group.name
   vpc_security_group_ids = [var.security_group_id]
 
   master_username = var.nexus_postgresql_database_username
