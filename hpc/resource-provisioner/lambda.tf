90ef17e19d56e7fc08ca4a062dc8d9cdf28373fd
Erik Heeren <erik.heeren@epfl.ch>
2024-07-01 12:02:14 +0200

    NOTICKET: hpc-resource-provisioner:latest
    And a mechanism to redeploy the lambdas if `latest` gets updated.

--- hpc/resource-provisioner/lambda.tf
+++ hpc/resource-provisioner/lambda.tf
@@ -1,3 +1,8 @@
+data "aws_ecr_image" "hpc_resource_provisioner_image" {
+  repository_name = "hpc-resource-provisioner"
+  image_tag       = "latest"
+}
+
 resource "aws_lambda_permission" "hpc_resource_provisioner_permission_post" {
   statement_id  = "AllowAPIGatewayInvokePOST"
   action        = "lambda:InvokeFunction"
@@ -8,13 +13,14 @@ resource "aws_lambda_permission" "hpc_resource_provisioner_permission_post" {
 
 # tfsec:ignore:aws-lambda-enable-tracing
 resource "aws_lambda_function" "hpc_resource_provisioner_lambda" {
-  function_name = "hpc-resource-provisioner"
-  role          = var.hpc_resource_provisioner_role
-  image_uri     = var.hpc_resource_provisioner_image_uri
-  package_type  = "Image"
-  architectures = ["x86_64"]
-  timeout       = 90
-  memory_size   = 1024
+  function_name    = "hpc-resource-provisioner"
+  role             = var.hpc_resource_provisioner_role
+  image_uri        = var.hpc_resource_provisioner_image_uri
+  package_type     = "Image"
+  architectures    = ["x86_64"]
+  timeout          = 90
+  memory_size      = 1024
+  source_code_hash = trimprefix(data.aws_ecr_image.hpc_resource_provisioner_image.id, "sha256:")
   vpc_config {
     security_group_ids = var.hpc_resource_provisioner_sg_ids
     subnet_ids         = var.hpc_resource_provisioner_subnet_ids
@@ -23,13 +29,14 @@ resource "aws_lambda_function" "hpc_resource_provisioner_lambda" {
 
 # tfsec:ignore:aws-lambda-enable-tracing
 resource "aws_lambda_function" "hpc_resource_provisioner_async_lambda" {
-  function_name = "hpc-resource-provisioner-creator"
-  role          = var.hpc_resource_provisioner_role
-  image_uri     = var.hpc_resource_provisioner_image_uri
-  package_type  = "Image"
-  architectures = ["x86_64"]
-  timeout       = 300
-  memory_size   = 1024
+  function_name    = "hpc-resource-provisioner-creator"
+  role             = var.hpc_resource_provisioner_role
+  image_uri        = var.hpc_resource_provisioner_image_uri
+  package_type     = "Image"
+  architectures    = ["x86_64"]
+  timeout          = 300
+  memory_size      = 1024
+  source_code_hash = trimprefix(data.aws_ecr_image.hpc_resource_provisioner_image.id, "sha256:")
   image_config {
     command = ["hpc_provisioner.handlers.pcluster_do_create_handler"]
   }
