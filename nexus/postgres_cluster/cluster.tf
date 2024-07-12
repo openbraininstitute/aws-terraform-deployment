814cb1878031e5f4a3aed0e46ccd1b4e1471e7a2
Simon Dumas <simon.dumas@epfl.ch>
2024-07-12 14:50:18 +0200

    NOTICKET Remove tags from postgres cluster

--- nexus/postgres_cluster/cluster.tf
+++ nexus/postgres_cluster/cluster.tf
@@ -1,10 +1,6 @@
 resource "aws_db_subnet_group" "nexus_cluster_subnet_group" {
   name       = "nexus-cluster-group"
   subnet_ids = var.subnets_ids
-  tags = {
-    SBO_Billing = "nexus"
-    Nexus       = "postgres"
-  }
 }
 
 # TODO the secret should be defined in the code
@@ -34,9 +30,4 @@ resource "aws_rds_cluster" "nexus" {
   master_password = data.aws_secretsmanager_secret_version.nexus_database_password.secret_string
 
   copy_tags_to_snapshot = true
-
-  tags = {
-    SBO_Billing = "nexus"
-    Nexus       = "postgres"
-  }
 }
