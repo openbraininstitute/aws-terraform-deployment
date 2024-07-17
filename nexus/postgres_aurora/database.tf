588bab9820da644bff5534ede0d5bafbc11df417
Simon Dumas <simon.dumas@epfl.ch>
2024-07-17 11:18:10 +0200

    NOTICKET Do not let Aurora manage the password

--- nexus/postgres_aurora/database.tf
+++ nexus/postgres_aurora/database.tf
@@ -26,8 +26,9 @@ module "aurora_postgresql" {
   backup_retention_period = 7 # in days
   storage_encrypted       = false
 
-  master_username = var.nexus_database_username
-  master_password = data.aws_secretsmanager_secret_version.nexus_database_password.secret_string
+  manage_master_user_password = false
+  master_username             = var.nexus_database_username
+  master_password             = data.aws_secretsmanager_secret_version.nexus_database_password.secret_string
 
   db_subnet_group_name = aws_db_subnet_group.nexus_aurora_subnet_group.name
 
