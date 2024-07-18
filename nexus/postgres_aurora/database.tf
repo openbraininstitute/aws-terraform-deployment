7f7ec1c0e4d4ec06326dc000db9bc9ca1405bf06
Simon Franck Benoit Dumas <simon.dumas@epfl.ch>
2024-07-18 13:41:06 +0000

    NOTICKET Attempt to fix Aurora deployment

--- nexus/postgres_aurora/database.tf
+++ nexus/postgres_aurora/database.tf
@@ -29,9 +29,9 @@ module "aurora_postgresql" {
   storage_encrypted       = false
 
   manage_master_user_password = false
-  master_username             = var.nexus_database_username
+  master_username             = "nexus_user"
   master_password             = data.aws_secretsmanager_secret_version.nexus_database_password.secret_string
-  database_name               = var.nexus_postgresql_database_name
+  #database_name               = var.nexus_postgresql_database_name
 
   db_subnet_group_name = aws_db_subnet_group.nexus_aurora_subnet_group.name
 
@@ -45,8 +45,7 @@ module "aurora_postgresql" {
     max_capacity = var.max_capacity
   }
 
-  skip_final_snapshot       = false
-  final_snapshot_identifier = "aurora-final-snapshot"
+  skip_final_snapshot       = true
 
   instance_class = "db.serverless"
   instances = {
