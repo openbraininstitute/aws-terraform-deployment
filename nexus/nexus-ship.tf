869a247e2c1567ceca5fc9b2b379a975b49f5f5e
Oliver Andrzej Grabinski <oliver.grabinski@epfl.ch>
2024-07-22 12:46:33 +0000

    NOTICKET use new db name, new target base for ship

--- nexus/nexus-ship.tf
+++ nexus/nexus-ship.tf
@@ -9,9 +9,9 @@ module "ship" {
   ecs_task_execution_role_arn = module.iam.nexus_ecs_task_execution_role_arn
   nexus_secrets_arn           = var.nexus_secrets_arn
   postgres_host               = "https://replace.this.postgres.host"
-  postgres_database           = "nexus-obp"
-  postgres_username           = "nexus-obp"
-  target_base_uri             = "https://sbo-nexus-delta.shapes-registry.org/v1"
+  postgres_database           = "nexusobp"
+  postgres_username           = "nexusobp"
+  target_base_uri             = "https://openbluebrain.com/api/delta/v1"
   target_bucket               = "nexus-obp-production"
   target_bucket_arn           = aws_s3_bucket.nexus_obp.arn
   second_target_bucket_arn    = aws_s3_bucket.nexus.arn
