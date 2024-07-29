8a9e8730e8356ade3c33cbfd2f24595540a38f4b
Oliver Andrzej Grabinski <oliver.grabinski@epfl.ch>
2024-07-29 12:03:28 +0000

    NOTICKET add nexus ship lambda

---
+++ nexus/ship/lambda_trigger_ecs.py
@@ -0,0 +1,57 @@
+import json
+import boto3
+
+ecs = boto3.client('ecs')
+
+def lambda_handler(event, context):
+
+    response = ecs.run_task(
+        cluster='nexus_ecs_cluster',
+        taskDefinition='nexus_ship_task_family',
+        launchType='FARGATE',
+        networkConfiguration={
+            'awsvpcConfiguration': {
+                'subnets': ['subnet-0ea70750833d08f39'], # nexus_a subnet
+                'securityGroups': ['sg-074f3f141431ea8b7'], # main_nexus_sg
+                'assignPublicIp': 'DISABLED'
+            }
+        },
+        overrides={
+            'containerOverrides': [
+                {
+                    'name': 'nexus_ship',
+                    'command': ['run','--s3','--config','ship.conf','--path', event['EXPORT_FILE_PATH']],
+                    'environment': [
+                        {
+                            'name': 'POSTGRES_DATABASE',
+                            'value': event['POSTGRES_DATABASE']
+                        },
+                        {
+                            'name': 'POSTGRES_HOST',
+                            'value': event['POSTGRES_HOST']
+                        },
+                        {
+                            'name': 'POSTGRES_USERNAME',
+                            'value': event['POSTGRES_USERNAME']
+                        },
+                        {
+                            'name': 'TARGET_BASE_URI',
+                            'value': event['TARGET_BASE_URI']
+                        },
+                        {
+                            'name': 'TARGET_BUCKET',
+                            'value': event['TARGET_BUCKET']
+                        }
+
+                    ]
+                }
+            ]
+        }
+    )
+
+    print(response)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('The lambda has reached its end')
+    }
