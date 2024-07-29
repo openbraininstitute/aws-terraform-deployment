8ad4fc159d6371ce702bb6790e17f04ae674d430
Oliver Andrzej Grabinski <oliver.grabinski@epfl.ch>
2024-07-29 15:33:04 +0000

    NOTICKET temporary fix for nexus ship lambda

--- nexus/ship/lambda.tf
+++ nexus/ship/lambda.tf
@@ -1,12 +1,22 @@
 data "archive_file" "python_lambda_package" {
   type        = "zip"
-  source_file = "${path.module}/lambda_trigger_ecs.py"
-  output_path = "${path.module}/ship_lambda.zip"
+  source_file = "${path.module}/lambda_function.py"
+  output_path = "${path.module}/lambda_package.zip"
+
+  depends_on = [random_string.r]
+}
+
+# This resource only exists to enforce the archive_file to be generated
+# during the apply part. See
+# https://github.com/hashicorp/terraform-provider-archive/issues/39#issuecomment-815021702
+resource "random_string" "r" {
+  length  = 16
+  special = false
 }
 
 resource "aws_lambda_function" "launch_ship" {
   function_name    = "nexus_launch_ship_task"
-  filename         = "${path.module}/ship_lambda.zip"
+  filename         = "${path.module}/lambda_package.zip"
   source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
   role             = aws_iam_role.nexus_ship_lambda.arn
   runtime          = "python3.12"
