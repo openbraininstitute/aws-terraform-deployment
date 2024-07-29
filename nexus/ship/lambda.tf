4cb174d345785f3d88c6053506d9f2d4ef924930
Oliver <oliver.grabinski@epfl.ch>
2024-07-29 14:25:26 +0200

    NOTICKET fix ship lambda path

--- nexus/ship/lambda.tf
+++ nexus/ship/lambda.tf
@@ -1,12 +1,12 @@
 data "archive_file" "python_lambda_package" {
   type        = "zip"
   source_file = "${path.module}/lambda_trigger_ecs.py"
-  output_path = "ship_lambda.zip"
+  output_path = "${path.module}/ship_lambda.zip"
 }
 
 resource "aws_lambda_function" "launch_ship" {
   function_name    = "nexus_launch_ship_task"
-  filename         = "ship_lambda.zip"
+  filename         = "${path.module}/ship_lambda.zip"
   source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
   role             = aws_iam_role.nexus_ship_lambda.arn
   runtime          = "python3.12"
