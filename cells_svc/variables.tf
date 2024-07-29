00af3fd2def137ee17fc657f1e0297cd05d76468
Mike Gevaert <michael.gevaert@epfl.ch>
2024-07-29 09:36:58 +0200

    NOTICKET: update sonata cell service

--- cells_svc/variables.tf
+++ cells_svc/variables.tf
@@ -13,7 +13,7 @@ variable "cell_svc_log_group_name" {
 }
 
 variable "cell_svc_docker_image_url" {
-  default     = "bluebrain/obp-sonata-cell-position@sha256:01d7c06b3486316b08c13f96a587282de47f92b967ced592fd8496fbea812b93"
+  default     = "bluebrain/obp-sonata-cell-position@sha256:6bc9c16b3423a484e5feb92070795f866bdc0de72c9ae27af776c7202d87d203"
   type        = string
   description = "docker image for the sonata-cell-service"
   sensitive   = false
