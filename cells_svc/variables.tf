49d21c29d4c8442207c74665702d7ea85f27a6fb
Gianluca Ficarelli <gianluca.ficarelli@epfl.ch>
2024-07-30 17:26:25 +0000

    NOTICKET cells_svc: update image to 2024.7.8

--- cells_svc/variables.tf
+++ cells_svc/variables.tf
@@ -13,7 +13,7 @@ variable "cell_svc_log_group_name" {
 }
 
 variable "cell_svc_docker_image_url" {
-  default     = "bluebrain/obp-sonata-cell-position@sha256:90428e7f704b67492937020227cc752125fa359bb9220cca70e8ab5de915c677"
+  default     = "bluebrain/obp-sonata-cell-position@sha256:56db61b02f8c4d658af38f126a23482d88af81ddeb259371dfb41374f2109de2"
   type        = string
   description = "docker image for the sonata-cell-service"
   sensitive   = false
