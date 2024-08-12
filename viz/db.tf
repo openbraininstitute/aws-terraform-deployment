382e46d893bd68f3244729748119241003f2ba69
Pawel Podhajski <pawel.podhajski@epfl.ch>
2024-08-12 13:53:50 +0000

    BBPP154-314 Add aws_dynamodb_table for VSM

--- viz/db.tf
+++ viz/db.tf
@@ -80,3 +80,26 @@ resource "aws_db_instance" "viz" {
     SBO_Billing = "viz"
   }
 }
+
+#tfsec:ignore:aws-dynamodb-table-customer-key
+resource "aws_dynamodb_table" "viz_vsm_jobs_table" {
+  name           = "viz-vsm-jobs-table"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  server_side_encryption {
+    enabled = true
+  }
+  point_in_time_recovery {
+    enabled = true
+  }
+  hash_key = "job_id"
+
+  attribute {
+    name = "job_id"
+    type = "S"
+  }
+  tags = {
+    SBO_Billing = "viz"
+  }
+}
