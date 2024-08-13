0f3b7370874a4cb6b494c791fd630a7d6dc3f1cb
Simon Dumas <simon.dumas@epfl.ch>
2024-08-13 11:56:27 +0200

    NOTICKET Fix postgres deployment

--- nexus/nexus-obp-new.tf
+++ nexus/nexus-obp-new.tf
@@ -1,18 +1,14 @@
-locals {
-  environment = "nexusobp"
-}
-
-module "postgres_cluster" {
+module "postgres_cluster_obp" {
   source = "./postgres_cluster"
 
   providers = {
     aws = aws.nexus_postgres_tags
   }
 
-  cluster_identifier = "nexus-obp-db"
-  subnets_ids        = module.networking.psql_subnets_ids
-  security_group_id  = module.networking.main_subnet_sg_id
-  instance_class     = "db.m5d.large"
+  cluster_identifier              = "nexus-obp-db"
+  subnets_ids                     = module.networking.psql_subnets_ids
+  security_group_id               = module.networking.main_subnet_sg_id
+  instance_class                  = "db.m5d.large"
   nexus_postgresql_engine_version = "16"
 
   aws_region = var.aws_region
