3ecd2f5b71b533e2c99c371a1e531066fce84b2d
Simon Dumas <simon.dumas@epfl.ch>
2024-08-15 12:14:27 +0200

    NOTICKET Remove old target groups based on hostname

--- nexus/delta_target_group/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "lb_target_group_arn" {
-  value = aws_lb_target_group.nexus_app.arn
-}
-
-output "hostname" {
-  value = var.nexus_delta_hostname
-}
--- nexus/fusion_target_group/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "lb_target_group_arn" {
-  value = aws_lb_target_group.nexus_fusion.arn
-}
-
-output "hostname" {
-  value = var.nexus_fusion_hostname
-}
