b85010cd71f9ad9ee9b1e310f67326f4ac9221db
Simon Franck Benoit Dumas <simon.dumas@epfl.ch>
2024-08-15 08:32:11 +0000

    NOTICKET Remove old Nexus deployment

--- nexus/target-groups.tf
+++ nexus/target-groups.tf
@@ -1,24 +1,3 @@
-module "sbo_delta_target_group" {
-  source = "./delta_target_group"
-
-  providers = {
-    aws = aws.nexus_delta_tags
-  }
-
-  nexus_delta_hostname     = "sbo-nexus-delta.shapes-registry.org"
-  target_group_prefix      = "nx-dlt"
-  unique_listener_priority = 100
-
-  vpc_id                        = var.vpc_id
-  domain_zone_id                = var.domain_zone_id
-  public_lb_listener_https_arn  = var.public_lb_listener_https_arn
-  public_load_balancer_dns_name = var.public_load_balancer_dns_name
-  nat_gateway_id                = var.nat_gateway_id
-  allowed_source_ip_cidr_blocks = var.allowed_source_ip_cidr_blocks
-
-  aws_region = var.aws_region
-}
-
 module "obp_delta_target_group" {
   source = "./path_target_group"
 
@@ -36,26 +15,6 @@ module "obp_delta_target_group" {
   unique_listener_priority      = 101
   nat_gateway_id                = var.nat_gateway_id
   vpc_id                        = var.vpc_id
-}
-
-module "sbo_fusion_target_group" {
-  source = "./fusion_target_group"
-
-  providers = {
-    aws = aws.nexus_fusion_tags
-  }
-
-  nexus_fusion_hostname    = "sbo-nexus-fusion.shapes-registry.org"
-  target_group_prefix      = "nx-fus"
-  unique_listener_priority = 300
-
-  aws_region                    = var.aws_region
-  vpc_id                        = var.vpc_id
-  domain_zone_id                = var.domain_zone_id
-  public_lb_listener_https_arn  = var.public_lb_listener_https_arn
-  public_load_balancer_dns_name = var.public_load_balancer_dns_name
-  nat_gateway_id                = var.nat_gateway_id
-  allowed_source_ip_cidr_blocks = var.allowed_source_ip_cidr_blocks
 }
 
 module "obp_fusion_target_group" {
