2676d51cd7f377782feb1c90af58bd6aa22f00a7
Sergio <sergio.rivasgomez@epfl.ch>
2024-08-20 23:51:10 +0000

    [BBPP154-279] Add support for ticket update with latest tagging report

--- .gitlab-ci-scripts/resource_tagging/3_generate_report.sh
+++ .gitlab-ci-scripts/resource_tagging/3_generate_report.sh
@@ -6,6 +6,9 @@ INPUT_FILE_U=${1:-"untagged_resources.csv"}
 INPUT_FILE_M=${2:-"modified_resources.csv"}
 OUTPUT_FILE_I=${3:-"ignored_resources.csv"}
 OUTPUT_FILE_T=${4:-"tagged_resources.csv"}
+TICKET_ID="BBPP154-279"
+COMMENT_ID="251281"
+AUTH_TOKEN="$(aws secretsmanager get-secret-value --secret-id "obp_bot_token" | jq -r ".SecretString")"
 TAGGED_RESOURCES=$(aws resource-explorer-2 search --query-string="region:us-east-1 tag.key:${TAG_KEY}")
 
 
@@ -40,16 +43,30 @@ done
 
 output_result_csv ${OUTPUT_FILE_T}
 
-# Finally, provide a summary of the results obtained
+# Calculate the number of resources for each category and generate the report
 num_resources_modified=$(sed 1d ${INPUT_FILE_M} 2>/dev/null | wc -l)
 num_resources_ignored=$(sed 1d ${OUTPUT_FILE_I} 2>/dev/null | wc -l)
 num_resources_untagged=$(sed 1d ${INPUT_FILE_U} 2>/dev/null | wc -l)
 num_resources_tagged=$(sed 1d ${OUTPUT_FILE_T}  2>/dev/null | wc -l)
-echo -ne "After running the tag verification scripts, here is a summary of the results:\n\n" \
-         " - ${num_resources_modified} resources have been modified and manually tagged with '${TAG_KEY}' and '${AUTOTAG_KEY}'.\n" \
-         " - ${num_resources_ignored} could not be verified or are purposely ignored (e.g., untaggeable resources managed by AWS).\n" \
-         " - ${num_resources_tagged} resources are properly tagged with '${TAG_KEY}' and accounted for.\n" \
-         " - ${num_resources_untagged} resources have been identified as '${TAG_KEY}' untagged"
-         [[ ${num_resources_untagged} -gt 0 ]] && echo ":" || echo "."
-         sed 1d ${INPUT_FILE_U} 2>/dev/null | cut -d${CSV_SEP} -f2 | uniq -c  # List number of untagged resources per owner
-echo -e "\nSee output CSV artifacts from the CI job for further information and specific details."
+report="$(
+    [[ ${num_resources_untagged} -gt 0 ]] && color="ff0000" || color="000000"
+    echo -n "{color:#${color}}*Current overall status of '{{${TAG_KEY}}}' tagging* [Updated: ${CI_JOB_STARTED_AT}]:{color}\n\n"
+    [[ ${num_resources_modified} -gt 0 ]] && echo -n " - ${num_resources_modified} resources have been modified and manually tagged with '{{${TAG_KEY}}}' and '{{${AUTOTAG_KEY}}}'.\n"
+    [[ ${num_resources_ignored}  -gt 0 ]] && echo -n " - ${num_resources_ignored} could not be verified or are purposely ignored (e.g., untaggeable resources managed by AWS).\n"
+    [[ ${num_resources_tagged}   -gt 0 ]] && echo -n " - ${num_resources_tagged} resources are properly tagged with '{{${TAG_KEY}}}' and accounted for.\n"
+
+    if [[ ${num_resources_untagged} -gt 0 ]]; then
+        echo -n " - ${num_resources_untagged} resources have been identified as '{{${TAG_KEY}}}' untagged:\n" \
+                "{code:bash}\n$(sed 1d ${INPUT_FILE_U} 2>/dev/null | cut -d${CSV_SEP} -f2 | uniq -c | tr '\n' '#' | sed 's|#|\\n|g'){code}\n" \
+                "\n*Please, it is advisable for every team to revise the latest CSV output for the untagged resources:*" \
+                "\nhttps://bbpgitlab.epfl.ch/cs/cloud/aws/deployment/-/jobs/${CI_JOB_ID}/artifacts/file/untagged_resources.csv"
+    else
+        echo -n "\n\nCurrently, no action is required by any team (i.e., all of the resources are properly identified)."
+    fi
+)"
+
+# Update the ticket with the latest information and print the summary without the Jira format
+curl -s -u obp_bot:${AUTH_TOKEN} -X PUT --data "{\"body\": \"${report}\"}" \
+                                 -H "Content-Type: application/json" \
+                                 https://bbpteam.epfl.ch/project/issues/rest/api/2/issue/${TICKET_ID}/comment/${COMMENT_ID} 1>/dev/null
+echo -e "${report}" | tr -d '*' | sed -r "s|'\{\{([^\}]+)\}\}'|'\1'|g" | sed -r "s|\{co[^\}]*}||g"
