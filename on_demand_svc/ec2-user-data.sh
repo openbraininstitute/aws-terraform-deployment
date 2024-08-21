afc76c535bc659cee9b48bd8039cefe6f1cc0403
genrich <genrich.ivaska@epfl.ch>
2024-08-21 10:01:05 +0200

    NOTICKET now fix single quotes

--- on_demand_svc/ec2-user-data.sh
+++ on_demand_svc/ec2-user-data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo ECS_CLUSTER=${ecs_cluster_name} >> /etc/ecs/ecs.config
-echo ECS_CONTAINER_INSTANCE_TAGS={${ecs_container_tags}} >> /etc/ecs/ecs.config
+echo 'ECS_CONTAINER_INSTANCE_TAGS={${ecs_container_tags}}' >> /etc/ecs/ecs.config
