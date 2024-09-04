3f97cf749dbe125ec226c09d98d6d3a5d184a0c7
Gianluca Ficarelli <gianluca.ficarelli@epfl.ch>
2024-09-04 09:23:41 +0000

    NOTICKET: bump to bluebrain/obp-sonata-cell-position:2024.9.1

--- cells_svc/variables.tf
+++ cells_svc/variables.tf
@@ -13,7 +13,7 @@ variable "cell_svc_log_group_name" {
 }
 
 variable "cell_svc_docker_image_url" {
-  default     = "bluebrain/obp-sonata-cell-position:2024.8.3"
+  default     = "bluebrain/obp-sonata-cell-position:2024.9.1"
   type        = string
   description = "docker image for the sonata-cell-service"
   sensitive   = false
