cd671bfc17f0f38b0d8baf18948b51676f644458
Mike Gevaert <michael.gevaert@epfl.ch>
2024-09-26 14:13:56 +0200

    NOTICKET: make sure that port 80 is available, so the http -> https works outside of EPFL network

--- subnets-public.tf
+++ subnets-public.tf
@@ -19,12 +19,12 @@ resource "aws_network_acl" "public" {
     from_port  = 0
     to_port    = 0
   }
-  # Allow port 80 from EPFL
+  # Allow port 80 from anywhere
   ingress {
     protocol   = "tcp"
     rule_no    = 105
     action     = "allow"
-    cidr_block = var.epfl_cidr
+    cidr_block = "0.0.0.0/0"
     from_port  = 80
     to_port    = 80
   }
