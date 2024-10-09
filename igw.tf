ba7f00f165ca6c0b456f7a1750e9056126318658
Daniel Fernandez <daniel.fernandez@epfl.ch>
2024-10-09 09:41:53 +0200

    BBPP154-304 Remove all commented out files

--- igw.tf
+++
@@ -1,7 +0,0 @@
-# Gateway for routed (not-NAT) traffic
-
-# Deprecated, use instead:
-
-# data.terraform_remote_state.common.outputs.internet_gateway_id
-# data.terraform_remote_state.common.outputs.internet_gateway_arn
-
--- nat-gateway.tf
+++
@@ -1,4 +0,0 @@
-# NAT gateway
-
-# Use:
-# data.terraform_remote_state.common.outputs.nat_gateway_id
