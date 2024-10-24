97ae7e2c4214a9fa4b5329d84a4c49fa4fdf84e2
Daniel Fernandez <daniel.fernandez@epfl.ch>
2024-10-24 12:21:47 +0200

    BBPP154-304 Create secret entry for core_webapp

--- core_webapp/core-webapp-secrets.tf
+++ core_webapp/core-webapp-secrets.tf
@@ -6,6 +6,12 @@ variable "sbo_core_webapp_secrets_arn" {
   sensitive   = false
 }
 
+#tfsec:ignore:aws-ssm-secret-use-customer-key
+resource "aws_secretsmanager_secret" "core_webapp_secrets" {
+  name        = "core_webapp_secrets"
+  description = "Core webapp secrets"
+}
+
 resource "aws_iam_policy" "sbo_core_webapp_secrets_access" {
   name        = "sbo-core-webapp-secrets-access-policy"
   description = "Policy that gives access to the SBO core webapp secrets"
