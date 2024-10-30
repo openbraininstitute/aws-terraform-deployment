8591e1b283efeaa25a8bb70fd110885c8a868844
Simon Dumas <simon.dumas@epfl.ch>
2024-10-30 09:51:50 +0100

    NOTICKET Deploy Delta 1.11-M4

--- nexus/nexus-obp.tf
+++ nexus/nexus-obp.tf
@@ -112,7 +112,7 @@ module "nexus_delta_obp_2" {
   delta_java_opts = "-Xss2m -Xms6g -Xmx6g"
 
   delta_instance_name        = "nexus-delta-obp-2"
-  delta_docker_image_version = "1.11.0-M1"
+  delta_docker_image_version = "1.11.0-M4"
   delta_efs_name             = "delta-obp-2"
   s3_bucket_arn              = aws_s3_bucket.nexus_obp.arn
   s3_bucket_name             = var.nexus_obp_bucket_name
