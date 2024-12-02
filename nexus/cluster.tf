175a40529f2f8b370db41fbc239c1b81fcc3b501
Simon Franck Benoit Dumas <simon.dumas@epfl.ch>
2024-12-02 12:28:18 +0000

    NOTICKET Add ECS cluster dedicated for Nexus openscience deployment

--- nexus/cluster.tf
+++ nexus/cluster.tf
@@ -10,3 +10,16 @@ resource "aws_service_discovery_http_namespace" "nexus" {
   name        = "nexus"
   description = "nexus service discovery namespace"
 }
+
+resource "aws_ecs_cluster" "nexus_openscience" {
+  name = "nexus_openscience_ecs_cluster"
+  setting {
+    name  = "containerInsights"
+    value = "disabled" #tfsec:ignore:aws-ecs-enable-container-insight
+  }
+}
+
+resource "aws_service_discovery_http_namespace" "nexus_openscience" {
+  name        = "nexus_openscience"
+  description = "nexus openscience service discovery namespace"
+}
