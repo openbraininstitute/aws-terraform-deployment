1880aed3063abdf6375234cdbca9828abdcd9f35
Daniel Fernández <daferoes@gmail.com>
2025-01-16 15:43:17 +0100

    Change terraform backend config from gitlab to AWS S3

--- remote.tf
+++ remote.tf
@@ -1,24 +1,10 @@
 data "terraform_remote_state" "common" {
-  backend = "http"
+  backend = "s3"
   config = {
-    address        = "https://bbpgitlab.epfl.ch/api/v4/projects/2295/terraform/state/default"
-    lock_address   = "https://bbpgitlab.epfl.ch/api/v4/projects/2295/terraform/state/default/lock"
-    lock_method    = "POST"
-    password       = var.common_token_pass
-    unlock_address = "https://bbpgitlab.epfl.ch/api/v4/projects/2295/terraform/state/default/lock"
-    username       = var.common_token_name
+    bucket         = var.terraform_remote_state_bucket_name
+    key            = "deployment-common/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = var.terraform_remote_state_dynamodb_table
+    encrypt        = true
   }
-}
-
-variable "common_token_name" {
-  description = "The token name needed to access the deployment-common repository."
-  type        = string
-  sensitive   = true
-
-}
-
-variable "common_token_pass" {
-  description = "The token password needed to access the deployment-common repository."
-  type        = string
-  sensitive   = true
 }
