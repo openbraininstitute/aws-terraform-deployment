3e98e262dfa4a2bb3de4fd122273d242c95aa7e3
Daniel Fernández <daferoes@gmail.com>
2025-01-28 08:29:19 +0100

    Move all bbp-workflow code under its own module
    Also use new workflow secret entry

--- bbp_workflow_svc/variables.tf
+++ bbp_workflow_svc/variables.tf
@@ -8,19 +8,14 @@ variable "aws_region" {
   description = "AWS region where the service will be deployed."
 }
 
-variable "ecs_subnet_id" {
-  type        = string
-  description = "Subnet where the service ECS components will be deployed."
-}
-
-variable "ecs_secgrp_id" {
-  type        = string
-  description = "Security group that the module will configure."
-}
-
 variable "account_id" {
   type        = string
   description = "AWS account id."
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "ID of the main VPC"
 }
 
 variable "svc_image" {
@@ -28,14 +23,17 @@ variable "svc_image" {
   description = "Docker image for the service."
 }
 
+variable "route_table_private_subnets_id" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string
+}
+
 variable "tags" {
   type        = map(string)
   description = "JSON schema for websocket incoming message validation."
-}
-
-variable "apigw_id" {
-  type        = string
-  description = "Tmp to get fixed url, remove."
 }
 
 variable "kc_scr" {
