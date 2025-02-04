ce04626a310612889f873b28fcd6c6411bf0a0f0
Daniel Fernández <daferoes@gmail.com>
2025-02-04 13:49:56 +0100

    Restrict security_groups and network ACLs ingress rules

--- cs/keycloak/security_groups.tf
+++ cs/keycloak/security_groups.tf
@@ -1,26 +1,38 @@
-# Create Security Group for EFS (Allowing all traffic)
+data "aws_vpc" "main" {
+  id = var.vpc_id
+}
+
 resource "aws_security_group" "efs_sg" {
   name        = "efs-sg"
-  description = "Security group for EFS"
+  description = "Security group for Keycloak related EFS"
   vpc_id      = var.vpc_id
-
-  # Allow all inbound traffic on NFS port (2049) from any source
-  ingress {
-    description = "Allow ingress from anywhere"
-    from_port   = 2049
-    to_port     = 2049
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-ingress-sgr
+  tags = {
+    SBO_Billing = "keycloak"
+    Name        = "keycloak_efs_sg"
   }
+}
 
-  # Allow all outbound traffic to any destination
-  egress {
-    description = "Allow egress to anywhere"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-egress-sgr
+resource "aws_vpc_security_group_egress_rule" "efs_sg_egress" {
+  security_group_id = aws_security_group.efs_sg.id
+  description       = "Allow egress to any destination"
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+  from_port         = 0
+  to_port           = 0
+
+  tags = {
+    SBO_Billing = "keycloak"
+    Name        = "keycloak_efs_sg"
   }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "efs_sg_ingress" {
+  security_group_id = aws_security_group.efs_sg.id
+  description       = "Allow ingress to NFS port from VPC"
+  cidr_ipv4         = data.aws_vpc.main.cidr_block
+  ip_protocol       = "tcp"
+  from_port         = 2049
+  to_port           = 2049
 
   tags = {
     SBO_Billing = "keycloak"
@@ -31,7 +43,7 @@ resource "aws_security_group" "efs_sg" {
 resource "aws_security_group" "main_sg" {
   vpc_id      = var.vpc_id
   name        = "keycloak_db_sg"
-  description = "main secruity group for keycloak db"
+  description = "Security group for keycloak db"
 
   tags = {
     SBO_Billing = "keycloak"
@@ -43,7 +55,7 @@ resource "aws_vpc_security_group_ingress_rule" "main_subnet_ingress" {
   security_group_id = aws_security_group.main_sg.id
   description       = "Allow everything incoming from the VPC"
   ip_protocol       = -1
-  cidr_ipv4         = "10.0.0.0/16"
+  cidr_ipv4         = data.aws_vpc.main.cidr_block
   from_port         = -1
   to_port           = -1
 
