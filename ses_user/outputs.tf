124e0c9509dadd05dcf321f76d0ed2ed5a9cce49
Dries Verachtert <dries.verachtert@dries.eu>
2025-02-06 13:33:27 +0100

    Fix SES password for sending emails

--- ses_user/outputs.tf
+++ ses_user/outputs.tf
@@ -4,8 +4,8 @@ output "access_key_id" {
   description = "Access Key ID for SES user"
 }
 
-output "secret_access_key" {
-  value       = aws_iam_access_key.ses_user_key.secret
-  description = "Secret Access Key for SES user"
+output "ses_smtp_password_v4" {
+  value       = aws_iam_access_key.ses_user_key.ses_smtp_password_v4
+  description = "Password for SMTP for SES user"
   sensitive   = true # Prevents accidental exposure
 }
