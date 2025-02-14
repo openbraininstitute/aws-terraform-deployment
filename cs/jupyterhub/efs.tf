51705c10e6d54052ea28df0d82f7cf2cb4747e55
Daniel Fernández <daferoes@gmail.com>
2025-02-14 11:22:33 +0100

    Enable backups for jupyterhub_homedirs EFS

--- cs/jupyterhub/efs.tf
+++ cs/jupyterhub/efs.tf
@@ -8,6 +8,14 @@ resource "aws_efs_file_system" "jupyterhub_homedirs" {
   }
 }
 
+resource "aws_efs_backup_policy" "jupyterhub_homedirs_backup_policy" {
+  file_system_id = aws_efs_file_system.jupyterhub_homedirs.id
+
+  backup_policy {
+    status = "ENABLED"
+  }
+}
+
 resource "aws_efs_mount_target" "jupyterhub_homedirs_mt" {
   file_system_id  = aws_efs_file_system.jupyterhub_homedirs.id
   security_groups = [aws_security_group.jupyterhub_efs_sg.id]
