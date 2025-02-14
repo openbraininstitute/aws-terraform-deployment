7ff4a8b431d14d85d445475a706b8afee6fe4499
Daniel Fernández <daferoes@gmail.com>
2025-02-14 11:20:43 +0100

    Fix jupyterhub_homedirs mount command

--- cs/jupyterhub/ec2_instance.tf
+++ cs/jupyterhub/ec2_instance.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "jupyterhub_server" {
 #!/bin/bash
 sudo apt update
 sudo apt install nfs-common -y
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 ${aws_efs_file_system.jupyterhub_homedirs.dns_name}:/ /home
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.jupyterhub_homedirs.dns_name}:/ /home
 EOF
 
   tags = {
