723fa2207eec485c73dab10baf286cd945c3fe6d
Nicolas Frank <58003267+WonderPG@users.noreply.github.com>
2025-02-18 18:32:29 +0100

    Change rate to retrigger task (#95)
    * revert real
    
    * Change rate

--- ml/producer.tf
+++ ml/producer.tf
@@ -14,7 +14,7 @@ module "ml_producer_eventbridge" {
     ml_producer_pmc = {
       description = "Cron for producer aimed at pmc's s3 bucket."
       # schedule_expression = "cron(0 2 * * ? *)"
-      schedule_expression = "rate(1 day)"
+      schedule_expression = "rate(3 day)"
       state               = "ENABLED"
     }
     ml_producer_local = {
