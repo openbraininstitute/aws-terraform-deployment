0e781eaf83c4d1cd984d9159e2617ff49ec831c8
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-02-19 15:43:52 +0100

    Remove dockerhub auth from core web app

--- core_webapp/core-webapp-container.tf
+++ core_webapp/core-webapp-container.tf
@@ -100,9 +100,7 @@ resource "aws_ecs_task_definition" "core_webapp_ecs_definition" {
       essential   = true
       image       = var.core_webapp_docker_image_url
       name        = "core_webapp"
-      repositoryCredentials = {
-        credentialsParameter = var.dockerhub_credentials_arn
-      }
+
       portMappings = [
         {
           hostPort      = 8000
@@ -297,12 +295,6 @@ EOF
   tags = {
     SBO_Billing = "core_webapp"
   }
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_core_webapp_task_role_dockerhub_policy_attachment" {
-  count      = var.core_webapp_ecs_number_of_containers > 0 ? 1 : 0
-  role       = aws_iam_role.ecs_core_webapp_task_execution_role[0].name
-  policy_arn = var.dockerhub_access_iam_policy_arn
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_core_webapp_secrets_access_policy_attachment" {
--- main.tf
+++ main.tf
@@ -251,8 +251,6 @@ module "core_webapp" {
   private_alb_https_listener_arn       = data.terraform_remote_state.common.outputs.private_alb_https_listener_arn
   aws_region                           = local.aws_region
   core_webapp_docker_image_url         = var.core_web_app_docker_image_url
-  dockerhub_access_iam_policy_arn      = local.dockerhub_bbpbuildbot_policy_arn
-  dockerhub_credentials_arn            = local.dockerhub_bbpbuildbot_secret_arn
   core_webapp_base_path                = "/app"
   route_table_id                       = local.route_table_private_subnets_id
   allowed_source_ip_cidr_blocks        = ["0.0.0.0/0"]
