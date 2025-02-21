5487320534c02b10d036cf53a7729d30ad9bb4b1
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-02-21 17:24:35 +0100

    Remove redirect to / -> /app for core web app

--- core_webapp/core-webapp-load-balancer.tf
+++ core_webapp/core-webapp-load-balancer.tf
@@ -38,37 +38,3 @@ resource "aws_lb_listener_rule" "private_core_webapp" {
     SBO_Billing = "core_webapp"
   }
 }
-
-# Generates a separate rule for each of the ranges in allowed_source_ip_cidr_blocks
-# => each individual rule remains below the 5 conditions limit
-resource "aws_lb_listener_rule" "private_core_webapp_redirect" {
-  # Generates a set [0, 1, 2, ..] with an index for each entry in var.cert_arns
-  for_each = toset(formatlist("%s", range(length(var.allowed_source_ip_cidr_blocks))))
-
-  listener_arn = var.private_alb_https_listener_arn
-  priority     = 250 + each.value
-
-  action {
-    type = "redirect"
-    redirect {
-      path        = "/app"
-      status_code = "HTTP_302"
-    }
-  }
-
-  condition {
-    path_pattern {
-      values = ["/", "/static/coming-soon/index.html"]
-    }
-  }
-
-  condition {
-    source_ip {
-      values = [var.allowed_source_ip_cidr_blocks[each.value]]
-    }
-  }
-
-  tags = {
-    SBO_Billing = "core_webapp"
-  }
-}
