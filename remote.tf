dd65a04960dfcfe0ec0dae531957056334f9c2fc
Daniel Fernández <daferoes@gmail.com>
2025-03-04 09:01:28 +0100

    Replace deprecated DynamoDB-based locking mechanism by S3 lockfile state (#144)

--- remote.tf
+++ remote.tf
@@ -1,10 +1,10 @@
 data "terraform_remote_state" "common" {
   backend = "s3"
   config = {
-    bucket         = var.terraform_remote_state_bucket_name
-    key            = "deployment-common/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = var.terraform_remote_state_dynamodb_table
-    encrypt        = true
+    bucket       = var.terraform_remote_state_bucket_name
+    key          = "deployment-common/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
