5f8585fe4d311b1be6bf6f56910f601eae0cf43e
Nicolas Frank <58003267+WonderPG@users.noreply.github.com>
2025-03-07 15:05:23 +0100

    bump up etl

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ module "ml" {
 
   dockerhub_credentials_arn = local.dockerhub_bbpbuildbot_secret_arn
   backend_image_tag         = "scholarag-v0.0.10"
-  etl_image_tag             = "scholaretl-v0.0.7"
+  etl_image_tag             = "scholaretl-v0.0.8"
   agent_image_tag           = "neuroagent-v0.3.3"
   grobid_image_url          = "lfoppiano/grobid:0.8.0"
 
