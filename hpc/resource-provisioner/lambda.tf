082df27d942433517ab25083ddac85e2a6333486
Erik Heeren <erik.heeren@openbraininstitute.org>
2025-03-07 10:35:44 +0100

    try without source_code_hash

--- hpc/resource-provisioner/lambda.tf
+++ hpc/resource-provisioner/lambda.tf
@@ -8,14 +8,13 @@ resource "aws_lambda_permission" "hpc_resource_provisioner_permission_post" {
 
 # tfsec:ignore:aws-lambda-enable-tracing
 resource "aws_lambda_function" "hpc_resource_provisioner_lambda" {
-  function_name    = "hpc-resource-provisioner"
-  role             = var.hpc_resource_provisioner_role
-  package_type     = "Image"
-  architectures    = ["x86_64"]
-  timeout          = 90
-  memory_size      = 1024
-  source_code_hash = trimprefix(data.aws_ecr_image.hpc_resource_provisioner_image.id, "sha256:")
-  image_uri        = data.aws_ecr_image.hpc_resource_provisioner_image.image_uri
+  function_name = "hpc-resource-provisioner"
+  role          = var.hpc_resource_provisioner_role
+  package_type  = "Image"
+  architectures = ["x86_64"]
+  timeout       = 90
+  memory_size   = 1024
+  image_uri     = data.aws_ecr_image.hpc_resource_provisioner_image.image_uri
   vpc_config {
     security_group_ids = var.hpc_resource_provisioner_sg_ids
     subnet_ids         = var.hpc_resource_provisioner_subnet_ids
@@ -30,14 +29,13 @@ data "aws_ecr_image" "hpc_resource_provisioner_image" {
 
 # tfsec:ignore:aws-lambda-enable-tracing
 resource "aws_lambda_function" "hpc_resource_provisioner_async_lambda" {
-  function_name    = "hpc-resource-provisioner-creator"
-  role             = var.hpc_resource_provisioner_role
-  package_type     = "Image"
-  architectures    = ["x86_64"]
-  timeout          = 300
-  memory_size      = 1024
-  source_code_hash = trimprefix(data.aws_ecr_image.hpc_resource_provisioner_image.id, "sha256:")
-  image_uri        = data.aws_ecr_image.hpc_resource_provisioner_image.image_uri
+  function_name = "hpc-resource-provisioner-creator"
+  role          = var.hpc_resource_provisioner_role
+  package_type  = "Image"
+  architectures = ["x86_64"]
+  timeout       = 300
+  memory_size   = 1024
+  image_uri     = data.aws_ecr_image.hpc_resource_provisioner_image.image_uri
   image_config {
     command = ["hpc_provisioner.handlers.pcluster_do_create_handler"]
   }
