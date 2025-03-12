905d2d2d99159de6c0238fb0fa7f6a3ab10ab517
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-03-12 09:31:39 +0100

    Update targets

--- scripts/terraform_plan_sandbox-nse.sh
+++ scripts/terraform_plan_sandbox-nse.sh
@@ -1,5 +1,7 @@
 echo "Terraform plan for NSE sandbox"
 
 terraform plan -target="module.entitycore_svc" \
+               -target=aws_db_instance.entitycore \
+              -target=aws_ecs_service.entitycore_ecs_service \
                -var-file=sandbox-nse.tfvars \
                -out plan.tfplan  && terraform show plan.tfplan
