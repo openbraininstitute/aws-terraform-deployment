8ce733a111a8279d20d367e24771ee3d5332ae5d
Jan Krepl <18519371+jankrepl@users.noreply.github.com>
2025-03-14 13:45:15 +0100

    Fix formatting

--- main.tf
+++ main.tf
@@ -91,10 +91,10 @@ module "ml" {
   agent_image_tag           = "neuroagent-v0.3.3"
   grobid_image_url          = "lfoppiano/grobid:0.8.0"
 
-  paper_bucket_name = var.ml_paper_bucket_name
+  paper_bucket_name      = var.ml_paper_bucket_name
   neuroagent_bucket_name = var.ml_neuroagent_bucket_name
-  nexus_domain_name = module.nexus.nexus_domain_name
-  primary_domain    = local.primary_domain
+  nexus_domain_name      = module.nexus.nexus_domain_name
+  primary_domain         = local.primary_domain
 
   # OLD PRIVATE ALB
   private_alb_security_group_id = data.terraform_remote_state.common.outputs.private_alb_security_group_id
