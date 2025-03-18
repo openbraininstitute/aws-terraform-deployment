89d1884927dfa1d42d5fbb3afdc96b3872524e78
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-03-18 15:27:09 +0100

    Correct auth URL for core web app next

--- main.tf
+++ main.tf
@@ -312,7 +312,7 @@ module "core_webapp_next" {
   secrets_arn                   = local.core_webapp_secrets_arn
   accounting_base_url           = "https://${local.primary_domain}${var.accounting_base_path}"
 
-  env_NEXTAUTH_URL                        = "https://${local.primary_domain}/api/auth"
+  env_NEXTAUTH_URL                        = "https://next.staging.openbraininstitute.org/api/auth"
   env_KEYCLOAK_ISSUER                     = "https://${local.primary_domain}/auth/realms/SBO"
   env_NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY  = "pk_test_51QjjHBKGUR5u3ofLgNUOpljnvy27UTTpkhwgsLiwK9xlNjnR7CZfiMjtZWMjgN7GW3eDyzMJ7Z1pIqC9LiwkfQRX00ebb5c9XI"
   env_NEXT_PUBLIC_BBS_ML_PRIVATE_BASE_URL = "http://${data.terraform_remote_state.common.outputs.private_alb_dns_name}:3000/api/literature"
