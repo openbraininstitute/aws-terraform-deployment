fa018687c78ef89b8c305f65fccaed59d5029a2e
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-03-18 14:13:12 +0100

    Remove reduntant comment

--- core_webapp/subnet.tf
+++ core_webapp/subnet.tf
@@ -1,5 +1,4 @@
 # Subnet for the core webapp
-# 10.0.2.0/28 is 10.0.2.0 up to 10.0.2.15 with subnet and broadcast included
 resource "aws_subnet" "core_webapp" {
   vpc_id                  = var.vpc_id
   availability_zone       = "${var.aws_region}a"
