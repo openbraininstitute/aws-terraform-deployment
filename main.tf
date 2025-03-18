dcb3859697ce086e10ce877ec7664ee2ab261ba8
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-03-18 11:21:08 +0100

    Make allowed_source_ip_cidr_blocks explicitly defined in main.tf

--- main.tf
+++ main.tf
@@ -266,17 +266,18 @@ module "static-server" {
 module "core_webapp_main" {
   source = "./core_webapp"
 
-  key                        = "main"
-  log_group_name             = "core_webapp_main"
-  vpc_id                     = local.vpc_id
-  alb_listener_arn           = data.terraform_remote_state.common.outputs.private_alb_https_listener_arn
-  alb_listener_rule_priority = 1000
-  aws_region                 = local.aws_region
-  docker_image_url           = var.core_web_app_docker_image_url
-  route_table_id             = local.route_table_private_subnets_id
-  vpc_cidr_block             = local.vpc_cidr_block
-  secrets_arn                = local.core_webapp_secrets_arn
-  accounting_base_url        = "https://${local.primary_domain}${var.accounting_base_path}"
+  key                           = "main"
+  log_group_name                = "core_webapp_main"
+  vpc_id                        = local.vpc_id
+  alb_listener_arn              = data.terraform_remote_state.common.outputs.private_alb_https_listener_arn
+  alb_listener_rule_priority    = 1000
+  allowed_source_ip_cidr_blocks = ["0.0.0.0/0"]
+  aws_region                    = local.aws_region
+  docker_image_url              = var.core_web_app_docker_image_url
+  route_table_id                = local.route_table_private_subnets_id
+  vpc_cidr_block                = local.vpc_cidr_block
+  secrets_arn                   = local.core_webapp_secrets_arn
+  accounting_base_url           = "https://${local.primary_domain}${var.accounting_base_path}"
 
   env_NEXTAUTH_URL                        = "https://${local.primary_domain}/api/auth"
   env_KEYCLOAK_ISSUER                     = "https://${local.primary_domain}/auth/realms/SBO"
@@ -299,14 +300,15 @@ module "core_webapp_next" {
   alb_listener_arn = data.terraform_remote_state.common.outputs.private_alb_https_listener_arn
   # The following priority has to be higher (lower number)
   # than the priority of the main core-web-app listener rule.
-  hostname                   = "next.staging.openbraininstitute.org"
-  alb_listener_rule_priority = 980
-  aws_region                 = local.aws_region
-  docker_image_url           = var.core_web_app_next_docker_image_url
-  route_table_id             = local.route_table_private_subnets_id
-  vpc_cidr_block             = local.vpc_cidr_block
-  secrets_arn                = local.core_webapp_secrets_arn
-  accounting_base_url        = "https://${local.primary_domain}${var.accounting_base_path}"
+  hostname                      = "next.staging.openbraininstitute.org"
+  alb_listener_rule_priority    = 980
+  allowed_source_ip_cidr_blocks = ["0.0.0.0/0"]
+  aws_region                    = local.aws_region
+  docker_image_url              = var.core_web_app_next_docker_image_url
+  route_table_id                = local.route_table_private_subnets_id
+  vpc_cidr_block                = local.vpc_cidr_block
+  secrets_arn                   = local.core_webapp_secrets_arn
+  accounting_base_url           = "https://${local.primary_domain}${var.accounting_base_path}"
 
   env_NEXTAUTH_URL                        = "https://${local.primary_domain}/api/auth"
   env_KEYCLOAK_ISSUER                     = "https://${local.primary_domain}/auth/realms/SBO"
