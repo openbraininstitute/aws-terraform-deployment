ee613afb6f537014784b49bae758047aa6bac860
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-03-19 12:00:30 +0100

    Enable on-demand svc from outside of EPFL network (#175)
    * Enable on-demand svc from outside of EPFL network
    
    * Use dummy-vlab-id as this is what it is

--- on_demand_svc/src/ws_authz.py
+++ on_demand_svc/src/ws_authz.py
@@ -1,19 +1,15 @@
-from ipaddress import ip_network, ip_address
-
-
 def do(event, context):
     headers = event["headers"]
-    ip = ip_address(headers["X-Forwarded-For"])
-    epfl_cidr = ip_network("128.178.0.0/15", False)
-    bbp_dmz_cidr = ip_network("192.33.211.0/26", False)
     token = headers.get("Sec-WebSocket-Protocol", "").replace("Bearer-", "Bearer ")
 
     # FIXME add proper keycloak authz
-    if ((ip in epfl_cidr or ip in bbp_dmz_cidr) and token.startswith("Bearer ")):
+    if token.startswith("Bearer "):
         authResponse = generateAllow("me", event["methodArn"])
         # FIXME get user vlab/proj
-        authResponse["context"] = {"SVC_VLAB": "O1_data_physiology_sep11",
-                                   "TOKEN": token}
+        authResponse["context"] = {
+            "SVC_VLAB": "dummy-vlab-id",
+            "TOKEN": token,
+        }
         return authResponse
 
     return generateDeny("me", event["methodArn"])
@@ -22,7 +18,7 @@ def do(event, context):
 def generatePolicy(principalId, effect, resource):
     authResponse = {}
     authResponse["principalId"] = principalId
-    if (effect and resource):
+    if effect and resource:
         policyDocument = {}
         policyDocument["Version"] = "2012-10-17"
         policyDocument["Statement"] = []
