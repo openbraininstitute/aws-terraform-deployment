7aefe502d440791752998cbde6f7624a01c12ebf
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-03-19 17:21:13 +0100

    Add endpoints

--- scripts/terraform_apply_sandbox-nse.sh
+++ scripts/terraform_apply_sandbox-nse.sh
@@ -2,6 +2,7 @@ echo "Terraform apply for NSE sandbox"
 
 terraform apply -auto-approve \
                 -target=module.entitycore_svc \
+                -target=module.networking \
                 -target=aws_db_instance.entitycore \
                 -target=aws_ecs_service.entitycore_ecs_service \
                 -var-file=sandbox-nse.tfvars
--- scripts/terraform_plan_sandbox-nse.sh
+++ scripts/terraform_plan_sandbox-nse.sh
@@ -1,6 +1,7 @@
 echo "Terraform plan for NSE sandbox"
 
 terraform plan -target="module.entitycore_svc" \
+               -target="module.networking" \
                -target=aws_db_instance.entitycore \
               -target=aws_ecs_service.entitycore_ecs_service \
                -var-file=sandbox-nse.tfvars \
