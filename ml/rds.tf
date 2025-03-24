88835a815e3ce45a2b7c2f6595788fe200508eff
Jan Krepl <18519371+jankrepl@users.noreply.github.com>
2025-03-24 14:39:25 +0100

    Disable rotation

--- ml/rds.tf
+++ ml/rds.tf
@@ -32,7 +32,7 @@ module "ml_rds_postgres" {
   port                        = var.rds_port
   skip_final_snapshot         = true
 
-  manage_master_user_password_rotation = false
+  manage_master_user_password_rotation = true
 
 
   vpc_security_group_ids = [aws_security_group.ml_rds.id]
