e6494426880a125bfbe8e30a2ff2175953a45391
Jan <18519371+jankrepl@users.noreply.github.com>
2025-03-24 14:46:06 +0100

    Enable DB pwd rotation (#190)

--- ml/rds.tf
+++ ml/rds.tf
@@ -32,7 +32,8 @@ module "ml_rds_postgres" {
   port                        = var.rds_port
   skip_final_snapshot         = true
 
-  manage_master_user_password_rotation = false
+  manage_master_user_password_rotation              = true
+  master_user_password_rotation_schedule_expression = "rate(15 days)"
 
 
   vpc_security_group_ids = [aws_security_group.ml_rds.id]
