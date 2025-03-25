a11e3fbea5a2ad745cdfdc78f4349cebfc327711
Daniel Fernández <daferoes@gmail.com>
2025-03-25 14:44:28 +0100

    Delete old elastic user credentials secret manager

--- nexus/elasticcloud/outputs.tf
+++ nexus/elasticcloud/outputs.tf
@@ -6,10 +6,6 @@ output "http_endpoint_ec2" {
   value = "https://${ec_deployment.deployment_ec2.alias}.es.${var.elastic_hosted_zone_name}"
 }
 
-output "elastic_user_credentials_secret_arn" {
-  value = aws_secretsmanager_secret.elastic_password.arn
-}
-
 output "elastic_user_credentials_secret_arn_ec2" {
   value = aws_secretsmanager_secret.elastic_password_ec2.arn
 }
