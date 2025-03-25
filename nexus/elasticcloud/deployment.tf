0b38975e0390ca04af005b7d13963bfc55e44875
Daniel Fernández <daferoes@gmail.com>
2025-03-25 14:44:28 +0100

    Delete old elastic user credentials secret manager

--- nexus/elasticcloud/deployment.tf
+++ nexus/elasticcloud/deployment.tf
@@ -38,17 +38,3 @@ resource "ec_deployment_traffic_filter" "deployment_filter" {
     source = var.elastic_vpc_endpoint_id
   }
 }
-
-#tfsec:ignore:aws-ssm-secret-use-customer-key
-resource "aws_secretsmanager_secret" "elastic_password" {
-  name                    = "nexus_${var.deployment_name}_elastic_password"
-  recovery_window_in_days = var.secret_recovery_window_in_days
-}
-
-resource "aws_secretsmanager_secret_version" "elastic_password" {
-  secret_id = aws_secretsmanager_secret.elastic_password.id
-  secret_string = jsonencode({
-    username = "elastic",
-    password = ec_deployment.deployment.elasticsearch_password
-  })
-}
