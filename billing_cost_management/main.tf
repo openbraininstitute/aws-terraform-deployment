14b344362726332f810adda46d290d2306eef441
Daniel Fernández <daferoes@gmail.com>
2025-03-31 11:50:36 +0200

    Delete billing_cost_management module
    Might be added again in the future but for now we decided to delete it
    https://github.com/openbraininstitute/INFRA/issues/129

--- billing_cost_management/main.tf
+++
@@ -1,6 +0,0 @@
-# Enable AWS Cost Optimization Hub, including all the accounts of the organization
-resource "aws_costoptimizationhub_enrollment_status" "aws_cost_opt_hub" {
-  count = var.is_production ? 1 : 0
-
-  include_member_accounts = true
-}
--- billing_cost_management/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "is_production" {
-  type        = bool
-  sensitive   = false
-  description = "Whether deployment is happening in production or not"
-}
-
