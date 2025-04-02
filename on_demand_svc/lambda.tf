e6f602bd3a9e80a0e2023d18b75fe5b7cde2fcb3
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-04-02 10:36:35 +0200

    Add resource to the existing list instead

--- on_demand_svc/lambda.tf
+++ on_demand_svc/lambda.tf
@@ -27,12 +27,7 @@ data "aws_iam_policy_document" "ws_handler_connect" {
   }
   statement {
     actions   = ["iam:PassRole"]
-    resources = [aws_iam_role.task_exec.arn]
-    effect    = "Allow"
-  }
-  statement {
-    actions   = ["iam:PassRole"]
-    resources = [aws_iam_role.task.arn]
+    resources = [aws_iam_role.task_exec.arn, aws_iam_role.task.arn]
     effect    = "Allow"
   }
 }
