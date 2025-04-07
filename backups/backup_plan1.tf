0f653868e1a17aa7729b4e706316e56ca588a198
Dries Verachtert <dries.verachtert@dries.eu>
2025-04-07 08:36:17 +0200

    Fix IAM role for backups

--- backups/backup_plan1.tf
+++ backups/backup_plan1.tf
@@ -11,22 +11,23 @@ resource "aws_backup_plan" "plan1" {
   }
 }
 
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["backup.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
 resource "aws_iam_role" "backup_role" {
   name = "backup_role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          type        = "Service"
-          identifiers = ["backup.amazonaws.com"]
-        }
-      }
-    ]
-  })
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
 
