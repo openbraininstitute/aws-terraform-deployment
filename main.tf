9105fec23db1d3dbab8a7c43597dc1c1a29527f0
Nicolas Frank <58003267+WonderPG@users.noreply.github.com>
2025-04-15 16:08:17 +0200

    Bump up ml version

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ module "ml" {
   dockerhub_credentials_arn = local.dockerhub_bbpbuildbot_secret_arn
   backend_image_tag         = "scholarag-v0.0.10"
   etl_image_tag             = "scholaretl-v0.0.8"
-  agent_image_tag           = "neuroagent-v0.4.6"
+  agent_image_tag           = "neuroagent-v0.4.7"
   grobid_image_url          = "lfoppiano/grobid:0.8.0"
 
   paper_bucket_name      = var.ml_paper_bucket_name
