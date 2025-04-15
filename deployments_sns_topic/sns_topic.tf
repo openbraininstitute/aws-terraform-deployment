4b77fb11db7f38a7f76d429b5401405b54fb9917
Dries Verachtert <dries.verachtert@dries.eu>
2025-04-15 11:20:54 +0200

    Give EventBridge access to the deployments sns topic

--- deployments_sns_topic/sns_topic.tf
+++ deployments_sns_topic/sns_topic.tf
@@ -6,11 +6,12 @@ resource "aws_cloudwatch_event_rule" "ecs_task_state_changes" {
   name        = "ecs-task-state-change-rule"
   description = "Triggers on ECS task RUNNING and STOPPED states"
   event_pattern = jsonencode({
-    source        = ["aws.ecs"],
-    "detail-type" = ["ECS Task State Change"],
-    detail = {
-      lastStatus = ["RUNNING", "STOPPED"]
-    }
+    source = ["aws.ecs"]
+    # source        = ["aws.ecs"],
+    # "detail-type" = ["ECS Task State Change"],
+    # detail = {
+    #   lastStatus = ["RUNNING", "STOPPED"]
+    # }
   })
 }
 
@@ -18,4 +19,32 @@ resource "aws_cloudwatch_event_target" "send_to_sns" {
   rule      = aws_cloudwatch_event_rule.ecs_task_state_changes.name
   target_id = "ecs-task-to-sns"
   arn       = aws_sns_topic.deployments.arn
+}
+
+data "aws_iam_policy_document" "allow_eventbridge_sns_topic" {
+  policy_id = "deployments_sns_topic_policy"
+
+  statement {
+    actions = [
+      "SNS:Publish",
+    ]
+
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    resources = [
+      aws_sns_topic.deployments.arn,
+    ]
+
+    sid = "deployments_sns_topic_policy"
+  }
+}
+
+resource "aws_sns_topic_policy" "allow_eventbridge" {
+  arn    = aws_sns_topic.deployments.arn
+  policy = data.aws_iam_policy_document.allow_eventbridge_sns_topic.json
 }
