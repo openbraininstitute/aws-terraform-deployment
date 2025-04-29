c9f5199191b6b3d488073ab5d26f0530350914e0
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-04-29 10:34:15 +0200

    Correct var rerefrence in entitycore module

--- entitycore_svc/s3.tf
+++ entitycore_svc/s3.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket_cors_configuration" "entitycore" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET", "HEAD"]
-    allowed_origins = s3_bucket_allowed_origins
+    allowed_origins = var.s3_bucket_allowed_origins
     expose_headers  = ["ETag", "Content-Length", "Content-Type", "Last-Modified"]
     max_age_seconds = 3000
   }
