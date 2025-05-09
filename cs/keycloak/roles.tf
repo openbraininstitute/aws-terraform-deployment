db58539246d4094d3b186ed00f76aa9aec2bb33a
Daniel Fernández <daferoes@gmail.com>
2025-05-09 12:15:33 +0200

    Add AmazonPrometheusRemoteWriteAccess to keycloak task

--- cs/keycloak/roles.tf
+++ cs/keycloak/roles.tf
@@ -98,6 +98,12 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attach
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+# to be able to send scraped metrics to Amazon Managed Service for Prometheus
+resource "aws_iam_role_policy_attachment" "ecs_task_prometheus_role_policy_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonPrometheusRemoteWriteAccess"
+}
+
 resource "aws_iam_role_policy_attachment" "ecs_task_cloudwatch_logs_attachment" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
