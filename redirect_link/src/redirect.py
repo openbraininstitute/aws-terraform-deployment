37e498931958cb32a66f0033714a99889170e5f7
Daniel Fernández <daferoes@gmail.com>
2025-06-05 15:48:42 +0200

    Redirect to jupyterhub_metabolism instance

--- redirect_link/src/redirect.py
+++ redirect_link/src/redirect.py
@@ -3,7 +3,7 @@ import logging
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
- 
+
 def lambda_redirect(event, context):
     logger.info(f"Received event: {json.dumps(event)}")
     # Extract request path
@@ -12,11 +12,11 @@ def lambda_redirect(event, context):
 
     # Define redirect rules
     redirect_map = {
-        "/doi/metabolism_shichkova_2025": "/jupyterhub/hub/user-redirect/git-pull?repo=https%3A%2F%2Fgithub.com%2Fopenbraininstitute%2Fobi_platform_analysis_notebooks&urlpath=lab%2Ftree%2Fobi_platform_analysis_notebooks%2FMetabolism%2Fanalysis_notebook.ipynb&branch=main"
+        "/doi/metabolism_shichkova_2025": "/jupyterhub_metabolism/hub/user-redirect/git-pull?repo=https%3A%2F%2Fgithub.com%2Fopenbraininstitute%2Fobi_platform_analysis_notebooks&urlpath=lab%2Ftree%2Fobi_platform_analysis_notebooks%2FMetabolism%2Fanalysis_notebook.ipynb&branch=main"
     }
 
     # Default: Polina'as notebook for now
-    redirect_url = redirect_map.get(path, "/jupyterhub/hub/user-redirect/git-pull?repo=https%3A%2F%2Fgithub.com%2Fopenbraininstitute%2Fobi_platform_analysis_notebooks&urlpath=lab%2Ftree%2Fobi_platform_analysis_notebooks%2FMetabolism%2Fanalysis_notebook.ipynb&branch=main")
+    redirect_url = redirect_map.get(path, "/jupyterhub_metabolism/hub/user-redirect/git-pull?repo=https%3A%2F%2Fgithub.com%2Fopenbraininstitute%2Fobi_platform_analysis_notebooks&urlpath=lab%2Ftree%2Fobi_platform_analysis_notebooks%2FMetabolism%2Fanalysis_notebook.ipynb&branch=main")
 
     return {
         "statusCode": 302,
