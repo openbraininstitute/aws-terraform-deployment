3f566f0ed1ef8ec5ed4b52c7286f03a6f5b7bf4d
Boris Bergsma <boris.bergsma@openbraininstitute.org>
2025-06-23 15:34:56 +0200

    neuroagent version v0.6.2

--- ml/agent.tf
+++ ml/agent.tf
@@ -82,8 +82,8 @@ module "ecs_service_agent" {
           value = "https://${var.primary_domain}/auth/realms/SBO"
         },
         {
-          name  = "NEUROAGENT_KNOWLEDGE_GRAPH__BASE_URL"
-          value = "https://${var.nexus_domain_name}/api/nexus/v1"
+          name  = "NEUROAGENT_LLM__WHITELISTED_MODEL_IDS_REGEX"
+          value = "openai/gpt-4.1-mini"
         },
         {
           name  = "NEUROAGENT_MISC__APPLICATION_PREFIX"
@@ -135,7 +135,7 @@ module "ecs_service_agent" {
         },
         {
           name  = "NEUROAGENT_TOOLS__WHITELISTED_TOOL_REGEX"
-          value = "literature-search-tool|web-search-tool"
+          value = "entitycore-asset-getall|entitycore-asset-getone|entitycore-brainatlas-getall|entitycore-brainatlas-getone|entitycore-brainregion-getall|entitycore-brainregion-getone|entitycore-brainregionhierarchy-getall|entitycore-brainregionhierarchy-getone|entitycore-contribution-getall|entitycore-contribution-getone|entitycore-electricalcellrecording-getall|entitycore-electricalcellrecording-getone|entitycore-emodel-getall|entitycore-emodel-getone|entitycore-etype-getall|entitycore-etype-getone|entitycore-experimentalboutondensity-getall|entitycore-experimentalboutondensity-getone|entitycore-experimentalneurondensity-getall|entitycore-experimentalneurondensity-getone|entitycore-experimentalsynapsesperconnection-getall|entitycore-experimentalsynapsesperconnection-getone|entitycore-memodel-getall|entitycore-memodel-getone|entitycore-mtype-getall|entitycore-mtype-getone|entitycore-organization-getall|entitycore-organization-getone|entitycore-person-getall|entitycore-person-getone|entitycore-reconstructionmorphology-getall|entitycore-reconstructionmorphology-getone|entitycore-species-getall|entitycore-species-getone|entitycore-strain-getall|entitycore-strain-getone|entitycore-subject-getall|entitycore-subject-getone|literature-search-tool|morpho-metrics-tool|web-search-tool"
         },
       ]
       secrets = [
@@ -144,7 +144,7 @@ module "ecs_service_agent" {
           valueFrom = "${module.ml_rds_postgres.db_instance_master_user_secret_arn}:password::"
         },
         {
-          name      = "NEUROAGENT_OPENAI__TOKEN"
+          name      = "NEUROAGENT_LLM__OPENAI_TOKEN"
           valueFrom = "${var.ml_secrets_arn}:OPENAI_API_KEY::"
         },
         {
