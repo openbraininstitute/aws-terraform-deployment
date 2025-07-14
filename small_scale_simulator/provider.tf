b56bffc04d791758460ab07184687e47a349f544
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-07-14 11:41:28 +0200

    Add billing tags for small scale simulator

---
+++ small_scale_simulator/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+}
+
+provider "aws" {
+  default_tags {
+    tags = {
+      SBO_Billing = "small_scale_simulator"
+    }
+  }
+  region = var.aws_region
+}
