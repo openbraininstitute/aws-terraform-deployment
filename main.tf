1ef3268113a43916f133726f30cef1c488e37afb
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-07-21 13:50:37 +0200

    Add missing cors origins for small scale simulator

--- main.tf
+++ main.tf
@@ -266,7 +266,8 @@ module "small_scale_simulator" {
   api_docker_image_url    = var.small_scale_simulator_api_docker_image_url
   worker_docker_image_url = var.small_scale_simulator_worker_docker_image_url
 
-  base_path = "/api/small-scale-simulator"
+  base_path    = "/api/small-scale-simulator"
+  cors_origins = ["https://${local.primary_domain}", var.is_staging ? "http://localhost:3000" : []]
 
   nexus_delta_uri = "https://${module.nexus.nexus_domain_name}/api/nexus/v1"
 
