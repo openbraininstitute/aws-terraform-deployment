bd17e827b4d6578e2da026253421a4aced1373af
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-07-24 14:21:42 +0200

    Put nse module back

---
+++ nse/main.tf
@@ -0,0 +1,13 @@
+module "me_model_analysis" {
+  source = "./me_model_analysis"
+
+  deployment_env = var.deployment_env
+
+  aws_region       = var.aws_region
+  account_id       = var.account_id
+  vpc_id           = var.vpc_id
+  docker_image_url = var.me_model_analysis_docker_image_url
+
+  amazon_linux_ecs_ami_id = var.amazon_linux_ecs_ami_id
+  route_table_id          = var.route_table_id
+}
---
+++ nse/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "account_id" {
+  description = "AWS account id."
+  type        = string
+}
+
+variable "me_model_analysis_docker_image_url" {
+  type = string
+}
+
+variable "amazon_linux_ecs_ami_id" {
+  type = string
+}
+
+variable "route_table_id" {
+  type = string
+}
+
+variable "deployment_env" {
+  type        = string
+  description = "The deployment environment, values: 'staging', 'production'"
+}
