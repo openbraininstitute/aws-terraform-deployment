e97ffc70cdfd5e98f40a6da93a29537eae1bc4ea
Daniel Fernández <daferoes@gmail.com>
2025-07-31 15:56:24 +0200

    Disable gathering kecyloak user event metrics

--- cs/keycloak/task.tf
+++ cs/keycloak/task.tf
@@ -84,7 +84,7 @@ resource "aws_ecs_task_definition" "sbo_keycloak_task" {
         },
         {
           name  = "KC_EVENT_METRICS_USER_ENABLED"
-          value = "true"
+          value = "false"
         },
         {
           name  = "KC_HTTP_METRICS_HISTOGRAMS_ENABLED"
