100d2d1bc44c41b637d191d258843a4d5d872408
Erik Heeren <erik.heeren@openbraininstitute.org>
2025-08-08 16:09:55 +0200

    Remove deprecated variable

--- hpc/resource-provisioner/lambda.tf
+++ hpc/resource-provisioner/lambda.tf
@@ -30,8 +30,6 @@ resource "aws_lambda_function" "hpc_resource_provisioner_lambda" {
       FSX_POLICY_ARN       = var.fsx_policy_arn
       FS_SUBNET_IDS        = jsonencode(var.fs_subnet_ids)
       FS_SG_ID             = var.fs_sg_id
-      # API_GW_STAGE_ARN     = aws_api_gateway_stage.hpc_resource_provisioner_api_stage.arn
-      API_GW_STAGE_ARN = "arn:aws:execute-api:${var.aws_region}:${var.account_id}:033upmtu0d/" # TODO: don't hardcode, find a way to do this without a cycle
     }
   }
 }
