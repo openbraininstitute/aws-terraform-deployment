0421de9a9e8c6b6180773687e80c3db276854dd3
Erik Heeren <erik.heeren@openbraininstitute.org>
2025-08-08 15:30:31 +0200

    Remove deprecated module from hpc plan

--- scripts/terraform_plan_sandbox-hpc.sh
+++ scripts/terraform_plan_sandbox-hpc.sh
@@ -4,7 +4,6 @@ terraform plan -target="module.hpc.module.vpc" \
                -target="module.hpc.module.security" \
                -target="module.hpc.module.networking" \
                -target="module.hpc.module.resource-provisioner" \
-               -target="module.hpc.module.resource-provisioner-dev" \
                -target="module.hpc.module.dynamodb" \
                -target="module.hpc.module.efs" \
                -target="module.coreservices_key" \
