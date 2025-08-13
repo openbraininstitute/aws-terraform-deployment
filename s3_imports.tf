d84c051c78054149f49730d15b8645785b393831
Erik Heeren <erik.heeren@openbraininstitute.org>
2025-08-13 16:06:41 +0200

    Don't import the policy

--- s3_imports.tf
+++ s3_imports.tf
@@ -2,8 +2,3 @@ import {
   id = trimprefix(var.infrastructureassets_bucket, "s3://")
   to = module.hpc.module.s3.aws_s3_bucket.sboinfrastructureassets
 }
-
-import {
-  id = trimprefix(var.infrastructureassets_bucket, "s3://")
-  to = module.hpc.module.s3.aws_s3_bucket_policy.sboinfrastructureassets
-}
