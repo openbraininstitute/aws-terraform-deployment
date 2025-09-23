0a177fb676a6ec39efd6884154675c80170a533f
Dries Verachtert <dries.verachtert@dries.eu>
2025-09-23 07:43:48 +0200

    Switch to JSON format for the list allowed CORS origins for the notebook service

--- variables-common.tf
+++ variables-common.tf
@@ -301,5 +301,5 @@ variable "notebook_hub_on_eks_full_url" {
 
 variable "notebook_service_cors_allowed_origins" {
   type        = string
-  description = "Comma separated list of allowed origins for the cors settings of the notebook service"
+  description = "JSON encoded list of allowed origins for the cors settings of the notebook service"
 }
