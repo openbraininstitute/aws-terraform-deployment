58ae7b48b70bf00c876523d02c3688bedc8ab4f0
Nicolas Frank <58003267+WonderPG@users.noreply.github.com>
2025-09-23 11:21:41 +0200

    Update neuroagent

--- main.tf
+++ main.tf
@@ -142,7 +142,7 @@ module "ml" {
   vpc_cidr_block                 = local.vpc_cidr_block
   route_table_private_subnets_id = local.route_table_private_subnets_id
 
-  agent_image_tag = "neuroagent-v0.8.1"
+  agent_image_tag = "neuroagent-v0.9.0"
 
   neuroagent_bucket_name = var.ml_neuroagent_bucket_name
   primary_domain         = local.primary_domain
