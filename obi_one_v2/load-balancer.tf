68cc79186039b4d43446b63ef751a2a2460387b2
Gianluca Ficarelli <gianluca.ficarelli@openbraininstitute.org>
2025-10-02 09:18:29 +0200

    Fix name prefixes

--- obi_one_v2/load-balancer.tf
+++ obi_one_v2/load-balancer.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "private_obi_one_v2" {
   #ts:skip=AC_AWS_0492
-  name_prefix = "obi_one_v2"
+  name_prefix = "obi-one-v2"
   port        = var.container_port
   protocol    = "HTTP"
   target_type = "ip"
