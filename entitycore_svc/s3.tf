b3346c3356773d05d6fac4aac00a896d924ff333
Erik Heeren <erik.heeren@openbraininstitute.org>
2025-10-13 17:39:14 +0200

    Enable versioning for entitycore bucket
    And set lifecycle configuration:
    * keep at least 3 versions
    * remove any extra noncurrent versions after 90 days

--- entitycore_svc/s3.tf
+++ entitycore_svc/s3.tf
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_cors_configuration" "entitycore" {
 resource "aws_s3_bucket_versioning" "entitycore" {
   bucket = aws_s3_bucket.entitycore.id
   versioning_configuration {
-    status = "Disabled"
+    status = "Enabled"
   }
 }
 
@@ -85,4 +85,20 @@ resource "aws_s3_bucket_policy" "prevent_delete" {
 resource "aws_s3_bucket_metric" "entitycore-metrics" {
   bucket = aws_s3_bucket.entitycore.id
   name   = "EntireBucket"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "entitycore" {
+  bucket = aws_s3_bucket.entitycore.id
+
+  rule {
+    id     = "remove-old-versions"
+    status = "Enabled"
+    filter {
+      prefix = ""
+    }
+    noncurrent_version_expiration {
+      noncurrent_days           = 90
+      newer_noncurrent_versions = 3
+    }
+  }
 }
