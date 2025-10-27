58580c3ea20ba1f184e4c5b98aa7eba719428319
Daniel Fernández <daferoes@gmail.com>
2025-10-27 09:30:52 +0100

    Remove jupyterhub EC2 based service

--- cs/main.tf
+++ cs/main.tf
@@ -26,27 +26,6 @@ module "jupyterhub_metabolism" {
   jupyterhub_listener_rule_priority = 350
 }
 
-module "jupyterhub" {
-  source                            = "./jupyterhub"
-  aws_coreservices_ssh_key_id       = var.aws_coreservices_ssh_key_id
-  vpc_id                            = var.vpc_id
-  private_alb_https_listener_arn    = var.private_alb_https_listener_arn
-  primary_domain                    = var.domain_name
-  jupyterhub_private_subnet         = module.networking.jupyterhub_private_subnet
-  jupyterhub_ec2_type               = var.jupyterhub_ec2_type
-  jupyterhub_nginx_port             = 80
-  jupyterhub_port                   = 8080
-  jupyterhub_base_path              = "/jupyterhub"
-  jupyterhub_secrets_arn            = var.jupyterhub_secrets_arn
-  jupyterhub_ec2_config_template    = "jupyterhub_config.sh.tpl"
-  jupyterhub_ec2_operating_system   = "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20250530"
-  jupyterhub_sg_name                = "jupyterhub-svc-sg"
-  jupyterhub_sg_efs_name            = "jupyterhub-svc-homedirs"
-  jupyterhub_target_group_name      = "jupyterhub-svc-tg"
-  jupyterhub_admin_users            = ["NataliBZ", "AurelienJaquier", "danielaegassan", "chr-pok", "dkeller9", "ilkilic", "james-isbister", "darshanmandge", "lidakanari", "mwolfr", "romani79", "danifr"]
-  jupyterhub_listener_rule_priority = 351
-}
-
 module "keycloak" {
   source                         = "./keycloak"
   private_subnets                = module.networking.keycloak_private_subnets
