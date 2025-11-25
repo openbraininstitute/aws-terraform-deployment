4fed9f2afe4619ef0a3079ba4201d876c1f4c746
Dries Verachtert <dries.verachtert@dries.eu>
2025-11-25 10:51:07 +0100

    Import the security group for the EFS homedirs mount targets

--- imports.tf
+++ imports.tf
@@ -1,4 +1,4 @@
 import {
-  id = "fs-05d5f395f0f265f91"
-  to = module.cs.module.jupyterhub_eks.aws_efs_file_system.users_homedirs
+  id = "sg-09c04ab62d5f18806"
+  to = module.cs.module.jupyterhub_eks.aws_security_group.efs_homedirs_sg
 }
