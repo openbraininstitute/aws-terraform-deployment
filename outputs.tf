6627559d1d148307d65ab23e3799e00e9e4996e9
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-11-28 15:29:13 +0100

    Core web app: clean up redeployment role for staging

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "github_core_web_app_main_ecs_redeploy_role_arn" {
-  value = var.is_staging ? module.github_core_webapp_main_ecs_redeploy_role[0].github_role_arn : null
-}
-
 output "github_core_web_app_dev_ecs_redeploy_role_arn" {
   value = var.is_staging ? module.github_core_webapp_dev_ecs_redeploy_role[0].github_role_arn : null
 }
