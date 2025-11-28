b366ed8bccf1e8e3ce5be03ae1093726503ed92a
Daniel Fernández <daferoes@gmail.com>
2025-11-28 12:09:51 +0100

    Change jupyterhub eks CIDRs

--- cs/main.tf
+++ cs/main.tf
@@ -34,8 +34,8 @@ module "jupyterhub_eks" {
 
   jupyterhub_eks_public_a_cidr  = "10.0.31.0/25"
   jupyterhub_eks_public_b_cidr  = "10.0.31.128/25"
-  jupyterhub_eks_private_a_cidr = "10.0.32.0/23"
-  jupyterhub_eks_private_b_cidr = "10.0.34.0/23"
+  jupyterhub_eks_private_a_cidr = "10.0.40.0/23"
+  jupyterhub_eks_private_b_cidr = "10.0.41.0/23"
 
   vpc_id         = var.vpc_id
   aws_region     = var.aws_region
