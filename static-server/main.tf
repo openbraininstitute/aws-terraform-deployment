0405aa639ec99eb5952d8be5712553a4756cc7ba
Daniel Fernández <daferoes@gmail.com>
2025-12-09 11:45:44 +0100

    Block all public access for static_storage S3 bucket

--- static-server/main.tf
+++ static-server/main.tf
@@ -96,13 +96,13 @@ resource "aws_s3_bucket_metric" "static_storage_metrics" {
   name   = "EntireBucket"
 }
 
-#tfsec:ignore:aws-s3-no-public-buckets
-#tfsec:ignore:aws-s3-block-public-policy
 resource "aws_s3_bucket_public_access_block" "static_storage" {
   bucket = aws_s3_bucket.static_storage.id
 
-  block_public_acls  = true
-  ignore_public_acls = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_policy" "static_storage" {
