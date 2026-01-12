a12f91dd0f7107ae0fca7e9563577bd48caf0876
Erik Heeren <erik.heeren@openbraininstitute.org>
2026-01-12 12:04:03 +0100

    Add cell-a to static server

--- main.tf
+++ main.tf
@@ -451,7 +451,8 @@ module "static-server" {
   account_id                 = local.account_id
   vpc_id                     = local.vpc_id
   public_subnet_ids          = [data.terraform_remote_state.common.outputs.public_a_subnet_id, data.terraform_remote_state.common.outputs.public_b_subnet_id]
-  domain_name                = local.old_primary_domain
+  old_domain_name            = local.old_primary_domain
+  domain_name                = local.cell_a_primary_domain
   static_content_bucket_name = local.old_primary_domain
   alb_listener_arn           = local.private_alb_https_listener_arn
   alb_listener_rule_priority = 600
