3532585cdcb3971e60e29d4932ddd35972a27822
Daniel Fernández <daferoes@gmail.com>
2026-01-20 13:22:39 +0100

    Import default HPC VPC SG into terraform code

--- hpc/outputs.tf
+++ hpc/outputs.tf
@@ -2,6 +2,10 @@ output "vpc_peering_security_group_id" {
   value = module.security.vpc_peering_security_group_id
 }
 
+output "pcluster_vpc_default_sg_id" {
+  value = module.vpc.pcluster_vpc_default_sg_id
+}
+
 output "resource_provisioner_api_url" {
   description = "The URL of the resource provisioner API"
   value       = module.resource-provisioner.api_url
--- imports.tf
+++ imports.tf
@@ -2,3 +2,8 @@ import {
   to = aws_default_security_group.default
   id = local.vpc_default_sg_id
 }
+
+import {
+  to = module.hpc.module.vpc.aws_default_security_group.hpc_default
+  id = module.hpc.pcluster_vpc_default_sg_id
+}
