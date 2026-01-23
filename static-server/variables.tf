863bf04fa7f1dd01866cecf5d4972bdec8035ff4
Dries Verachtert <dries.verachtert@dries.eu>
2026-01-23 16:23:36 +0100

    Static files only reachable on the 'new' cell-a URLs, remove the old hostname
    https://github.com/openbraininstitute/INFRA/issues/373

--- static-server/variables.tf
+++ static-server/variables.tf
@@ -8,11 +8,6 @@ variable "account_id" {
   type        = string
 }
 
-variable "old_domain_name" {
-  description = "The old domain name for the static website"
-  type        = string
-}
-
 variable "domain_name" {
   description = "The domain name for the static website"
   type        = string
