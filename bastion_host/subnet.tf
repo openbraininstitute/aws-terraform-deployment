3b18008659f563a1dc2e60a95bddb45f07347d4d
Daniel Fernández <daferoes@gmail.com>
2026-01-29 11:10:36 +0100

    Use a different network range for SSM bastion service

--- bastion_host/subnet.tf
+++ bastion_host/subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "bastion" {
   vpc_id            = var.vpc_id
   availability_zone = "${data.aws_region.current.name}a"
-  cidr_block        = "10.0.31.0/28"
+  cidr_block        = "10.0.3.0/28"
   tags = {
     Name = "bastion_host"
   }
