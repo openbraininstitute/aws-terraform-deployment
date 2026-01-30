c42ae6ced45cfb6d332003c31de48a5f8f37eda2
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2026-01-30 15:02:09 +0100

    Fix core web app preview deployments (#965)
    * Enable auto subdomain creation
    
    * Fix iam role trust relationship
    
    * Restrict only to a particular hosted zone
    
    * Format

--- core_webapp_preview/main.tf
+++ core_webapp_preview/main.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_iam_role" "amplify_service" {
   name = "${var.app_name}-amplify-service-role"
 
@@ -9,6 +11,14 @@ resource "aws_iam_role" "amplify_service" {
         Service = "amplify.amazonaws.com"
       }
       Action = "sts:AssumeRole"
+      Condition = {
+        StringEquals = {
+          "aws:SourceAccount" = data.aws_caller_identity.current.account_id
+        }
+        ArnLike = {
+          "aws:SourceArn" = "arn:aws:amplify:*:${data.aws_caller_identity.current.account_id}:apps/*"
+        }
+      }
     }]
   })
 }
@@ -19,13 +29,28 @@ resource "aws_iam_role_policy" "amplify_service" {
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "secretsmanager:GetSecretValue"
-      ]
-      Resource = var.secrets_arn
-    }]
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ]
+        Resource = var.secrets_arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "route53:ChangeResourceRecordSets",
+          "route53:ListResourceRecordSets"
+        ]
+        Resource = "arn:aws:route53:::hostedzone/${var.route53_zone_id}"
+      },
+      {
+        Effect   = "Allow"
+        Action   = "route53:ListHostedZones"
+        Resource = "*"
+      }
+    ]
   })
 }
 
@@ -50,7 +75,8 @@ resource "aws_amplify_app" "this" {
   }
 
   enable_branch_auto_build    = true
-  enable_auto_branch_creation = false
+  enable_auto_branch_creation = true
+  enable_branch_auto_deletion = true
 }
 
 resource "aws_amplify_branch" "default" {
@@ -65,9 +91,21 @@ resource "aws_amplify_domain_association" "this" {
   domain_name = var.domain_name
 
   sub_domain {
-    branch_name = aws_amplify_branch.default.branch_name
+    branch_name = "main"
     prefix      = "main"
   }
+
+  sub_domain {
+    branch_name = "develop"
+    prefix      = "dev"
+  }
+
+  sub_domain {
+    branch_name = "*"
+    prefix      = ""
+  }
+
+  enable_auto_sub_domain = true
 }
 
 resource "aws_iam_role" "github_deploy" {
