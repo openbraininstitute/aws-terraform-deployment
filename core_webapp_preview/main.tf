6469a80b97606a85c5dfce9f2e660a7a5932f1dd
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2026-01-30 15:13:49 +0100

    Remove incorrect auto-branch creation definition (#968)
    * Remove incorrect auto-branch creation definition
    
    * Update auto branch creation pattern

--- core_webapp_preview/main.tf
+++ core_webapp_preview/main.tf
@@ -77,6 +77,8 @@ resource "aws_amplify_app" "this" {
   enable_branch_auto_build    = true
   enable_auto_branch_creation = true
   enable_branch_auto_deletion = true
+
+  auto_branch_creation_patterns = ["*"]
 }
 
 resource "aws_amplify_branch" "default" {
@@ -98,11 +100,6 @@ resource "aws_amplify_domain_association" "this" {
   sub_domain {
     branch_name = "develop"
     prefix      = "dev"
-  }
-
-  sub_domain {
-    branch_name = "*"
-    prefix      = ""
   }
 
   enable_auto_sub_domain = true
