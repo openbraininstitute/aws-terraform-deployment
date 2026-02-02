41f28a32be8d1e3a0d5de865050382b654859fb8
Daniel Fernández <daferoes@gmail.com>
2026-02-02 14:46:59 +0100

    Add list of required packages to SSM bastion node

--- bastion_host/scripts/user_setup.sh
+++ bastion_host/scripts/user_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Install required packages
 dnf update -y
-dnf install -y jq curl sudo
+dnf install -y curl git joe jq lsof nmap nmap-ncat postgresql17 postgresql17-libs rsync strace sudo tcpdump tmux traceroute vim wget zsh
 
 # Install SSM Agent for Amazon Linux 2023
 dnf install -y amazon-ssm-agent
