d646af0761a34d008d1ccfd9a8228a78d54bc480
Daniel Fernández <daferoes@gmail.com>
2026-02-09 16:08:51 +0100

    Enable readonlyRootFilesystem for entitycore service

--- entitycore_svc/backend.tf
+++ entitycore_svc/backend.tf
@@ -87,6 +87,16 @@ resource "aws_ecs_task_definition" "entitycore_ecs_definition" {
 
       essential = true
 
+      readonlyRootFilesystem = true
+
+      mountPoints = [
+        {
+          sourceVolume  = "tmp"
+          containerPath = "/tmp"
+          readOnly      = false
+        }
+      ]
+
       portMappings = [
         {
           hostPort      = 8000
@@ -192,6 +202,10 @@ resource "aws_ecs_task_definition" "entitycore_ecs_definition" {
 
   cpu    = local.cpu
   memory = local.memory
+
+  volume {
+    name = "tmp"
+  }
 
   requires_compatibilities = ["FARGATE"]
 
