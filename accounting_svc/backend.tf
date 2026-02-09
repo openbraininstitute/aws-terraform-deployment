8f35f66782aae5bbede5c276ee90c9b05fd7f87a
Daniel Fernández <daferoes@gmail.com>
2026-02-09 15:34:50 +0100

    Enable readonlyRootFilesystem for accounting service

--- accounting_svc/backend.tf
+++ accounting_svc/backend.tf
@@ -87,6 +87,16 @@ resource "aws_ecs_task_definition" "accounting_ecs_definition" {
 
       essential = true
 
+      readonlyRootFilesystem = true
+
+      mountPoints = [
+        {
+          sourceVolume  = "tmp"
+          containerPath = "/tmp"
+          readOnly      = false
+        }
+      ]
+
       portMappings = [
         {
           hostPort      = 8000
@@ -167,6 +177,10 @@ resource "aws_ecs_task_definition" "accounting_ecs_definition" {
 
   cpu    = local.cpu
   memory = local.memory
+
+  volume {
+    name = "tmp"
+  }
 
   requires_compatibilities = ["FARGATE"]
 
