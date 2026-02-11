d2b597a1072ff407c6100dcc0e766754f4844498
Daniel Fernández <daferoes@gmail.com>
2026-02-11 12:44:24 +0100

    Enable readonlyRootFilesystem for launch-system API service

--- launch_system/api.tf
+++ launch_system/api.tf
@@ -82,6 +82,16 @@ resource "aws_ecs_task_definition" "api" {
 
       essential = true
 
+      readonlyRootFilesystem = true
+
+      mountPoints = [
+        {
+          sourceVolume  = "tmp"
+          containerPath = "/tmp"
+          readOnly      = false
+        }
+      ]
+
       portMappings = [
         {
           hostPort      = 8000
@@ -227,6 +237,10 @@ resource "aws_ecs_task_definition" "api" {
 
   cpu    = var.api_task_size.cpu
   memory = var.api_task_size.memory
+
+  volume {
+    name = "tmp"
+  }
 
   requires_compatibilities = ["FARGATE"]
 
