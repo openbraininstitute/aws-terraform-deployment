e5574fbc87231c98ba142bddc5e02f43a6170178
Erik Heeren <erik.heeren@openbraininstitute.org>
2026-02-20 17:15:54 +0100

    Don't sync to NFS on a schedule for now

--- public_data_sync_opendata/datasync.tf
+++ public_data_sync_opendata/datasync.tf
@@ -319,12 +319,6 @@ resource "aws_datasync_task" "opendata_s3_to_azure_nfs" {
     object_tags            = "NONE"
   }
 
-  schedule {
-    # apparently you can't have `*` in both day-of-month and day-of-week - one needs to be a ? instead
-    # minute | hour | day of month | month | day of week | year
-    schedule_expression = "cron(0 0 ? * * *)"
-  }
-
   task_mode = "BASIC"
 }
 
@@ -345,12 +339,6 @@ resource "aws_datasync_task" "internal_s3_to_azure_nfs" {
     preserve_devices       = "NONE"
     bytes_per_second       = -1 # unlimited
     object_tags            = "NONE"
-  }
-
-  schedule {
-    # apparently you can't have `*` in both day-of-month and day-of-week - one needs to be a ? instead
-    # minute | hour | day of month | month | day of week | year
-    schedule_expression = "cron(0 0 ? * * *)"
   }
 
   task_mode = "BASIC"
