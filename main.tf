814ed5e7d5f62e6ebeb208b458a2c63c8fb95c77
Boris Bergsma <boris.bergsma@openbraininstitute.org>
2026-02-26 12:17:30 +0100

    Fix the s3 bucket CORS for preview envs

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ locals {
       "http://127.0.0.1:3000",
       "http://localhost:3000",
       "https://preview.openbraininstitute.org",
+      "https://*.preview.openbrainionstitute.org",
       "https://dev.openbraininstitute.org",
       "https://staging.cell-b.openbraininstitute.org",
       "https://staging.cell-a.openbraininstitute.org"
