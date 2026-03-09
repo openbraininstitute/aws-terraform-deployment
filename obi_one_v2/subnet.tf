7e1ca1ccf7d9ae355a5ee2d64c7646a8cf3983eb
Daniel Fernández <daferoes@gmail.com>
2026-03-09 15:29:44 +0100

    Allow obi-one access from all origins on ephemeral ports

--- obi_one_v2/subnet.tf
+++ obi_one_v2/subnet.tf
@@ -27,9 +27,17 @@ resource "aws_network_acl" "obi_one_v2" {
   }
   ingress {
     protocol   = "tcp"
+    rule_no    = 105
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 3389
+    to_port    = 3389
+  }
+  ingress {
+    protocol   = "tcp"
     rule_no    = 110
     action     = "allow"
-    cidr_block = var.vpc_cidr_block
+    cidr_block = "0.0.0.0/0"
     from_port  = 1024
     to_port    = 65535
   }
