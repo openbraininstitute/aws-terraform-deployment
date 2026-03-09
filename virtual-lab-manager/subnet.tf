4ff5b86efc16dda10a422bc24edb0598d0ba85b7
Daniel Fernández <daferoes@gmail.com>
2026-03-09 14:48:42 +0100

    Remove unnecessarily open network acls

--- virtual-lab-manager/subnet.tf
+++ virtual-lab-manager/subnet.tf
@@ -38,7 +38,6 @@ resource "aws_route_table_association" "virtual_lab_manager_b" {
 resource "aws_network_acl" "core_svc" {
   vpc_id     = var.vpc_id
   subnet_ids = [aws_subnet.virtual_lab_manager_a.id, aws_subnet.virtual_lab_manager_b.id]
-  # Allow local traffic
   ingress {
     protocol   = -1
     rule_no    = 100
@@ -47,36 +46,15 @@ resource "aws_network_acl" "core_svc" {
     from_port  = 0
     to_port    = 0
   }
-  /* # Allow temporarily all
-  ingress {
-    protocol = -1
-    rule_no = 101
-    action = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port = 0
-    to_port = 0
-  }*/
-  # Allow port 8000 from anywhere
-  # TODO limit to just ALB?
-  ingress {
-    protocol   = "tcp"
-    rule_no    = 105
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 8000
-    to_port    = 8000
-  }
-  # allow ingress ephemeral ports
   ingress {
     protocol   = "tcp"
     rule_no    = 300
     action     = "allow"
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.vpc_cidr_block
     from_port  = 1024
     to_port    = 65535
   }
   egress {
-    # TODO limit to dockerhub, secretsmanager, nexus...
     protocol   = -1
     rule_no    = 100
     action     = "allow"
