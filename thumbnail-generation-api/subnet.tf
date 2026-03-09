cc1bcabdd3cb15ffbc83d95e2297e5c384bc8f5e
Daniel Fernández <daferoes@gmail.com>
2026-03-09 15:32:26 +0100

    Allow thumbnail-generation-api access from all origins on ephemeral ports

--- thumbnail-generation-api/subnet.tf
+++ thumbnail-generation-api/subnet.tf
@@ -30,9 +30,17 @@ resource "aws_network_acl" "main" {
   }
   ingress {
     protocol   = "tcp"
+    rule_no    = 200
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 3389
+    to_port    = 3389
+  }
+  ingress {
+    protocol   = "tcp"
     rule_no    = 300
     action     = "allow"
-    cidr_block = var.vpc_cidr_block
+    cidr_block = "0.0.0.0/0"
     from_port  = 1024
     to_port    = 65535
   }
