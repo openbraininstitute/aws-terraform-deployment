32e58a235059fcfb7b18e37b26f741464f3bcca1
MikeG <mgevaert@openbraininstitute.org>
2026-03-11 16:27:59 +0100

    Update main.tf
    Co-authored-by: Erik Heeren <erik.heeren@epfl.ch>

--- main.tf
+++ main.tf
@@ -976,7 +976,7 @@ module "launch_system" {
   local_store_prefix        = "/data"
   simulation_launch_command = "/data/scratch/run-simulation-venv/bin/python3 /data/scratch/run_simulation.py"
 
-  pcs_ami            = ""
+  pcs_ami            = "ami-05b075d72e0d560fa"
   pcs_nat_gateway_id = data.terraform_remote_state.common.outputs.nat_gateway_id
   pcs_cidr_block     = "10.0.31.0/24"
 
