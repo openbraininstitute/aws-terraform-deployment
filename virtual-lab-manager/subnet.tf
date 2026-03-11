71537351f3be52866dd49c10cefe1265c2077a66
Dries Verachtert <dries.verachtert@dries.eu>
2026-03-11 14:23:08 +0100

    Allow ephemeral return ports for tcp traffic from anywhere for VLM, fixes access to ECR repositories for fetching container image
    https://github.com/openbraininstitute/INFRA/issues/471

--- virtual-lab-manager/subnet.tf
+++ virtual-lab-manager/subnet.tf
@@ -46,11 +46,20 @@ resource "aws_network_acl" "core_svc" {
     from_port  = 0
     to_port    = 0
   }
+  # Deny RDP access
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 150
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 3389
+    to_port    = 3389
+  }
   ingress {
     protocol   = "tcp"
     rule_no    = 300
     action     = "allow"
-    cidr_block = var.vpc_cidr_block
+    cidr_block = "0.0.0.0/0"
     from_port  = 1024
     to_port    = 65535
   }
