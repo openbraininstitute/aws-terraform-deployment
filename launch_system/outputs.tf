164800344aaa80c40a88060282fa39abe9531a1d
Mike Gevaert <mgevaert@openbraininstitute.org>
2026-03-12 10:39:07 +0100

    Revert "launch-system: move `launch_system_network` back into `launch_system` (#1159)"
    This reverts commit e314013722d444cf7bf1279b9a01db71dc140dad.

--- launch_system/outputs.tf
+++ launch_system/outputs.tf
@@ -2,11 +2,3 @@ output "private_lb_rule_suffix" {
   description = "Launch System Private Loadbalancer Rule Suffix"
   value       = aws_lb_target_group.private.arn_suffix
 }
-
-output "executor_network_ids" {
-  description = "Subnet ids where the Launch System Executor is deployed"
-  value = [
-    aws_subnet.untrusted_a.id,
-    aws_subnet.untrusted_b.id,
-  ]
-}
