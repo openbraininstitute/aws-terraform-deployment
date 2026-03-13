4d2a2fcde3eb16c2e43753e33c0b701a5beffd79
Mike Gevaert <mgevaert@openbraininstitute.org>
2026-03-13 15:46:09 +0100

    add slurmrestd

--- launch_system/security-groups.tf
+++ launch_system/security-groups.tf
@@ -51,6 +51,15 @@ resource "aws_vpc_security_group_ingress_rule" "ssh" {
   cidr_ipv4         = var.vpc_cidr_block
 }
 
+resource "aws_vpc_security_group_ingress_rule" "slurmrestd" {
+  security_group_id = aws_security_group.pcs.id
+  description       = "slurmrestd"
+  ip_protocol       = "tcp"
+  from_port         = 6820
+  to_port           = 6820
+  cidr_ipv4         = var.vpc_cidr_block
+}
+
 resource "aws_vpc_security_group_ingress_rule" "lustre_lnet" {
   security_group_id = aws_security_group.pcs.id
   description       = "Lustre LNET network traffic"
