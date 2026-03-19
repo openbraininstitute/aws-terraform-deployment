70ec716e6949ea356b8f5181447b2f777b635997
Daniel Fernández <daferoes@gmail.com>
2026-03-19 14:35:06 +0100

    Upgrade Keycloak postgresql engine to v17

--- cs/keycloak/database.tf
+++ cs/keycloak/database.tf
@@ -46,7 +46,7 @@ resource "aws_db_instance" "keycloak_database" {
   allocated_storage            = 20
   storage_type                 = "gp2"
   engine                       = "postgres"
-  engine_version               = "16"
+  engine_version               = "17"
   auto_minor_version_upgrade   = true
   instance_class               = var.db_instance_class
   db_name                      = "keycloak"
