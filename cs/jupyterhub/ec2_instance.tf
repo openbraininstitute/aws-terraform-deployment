6d9aeec51b6afc1ca538bd75c407a871fd157d2d
Daniel Fernández <daferoes@gmail.com>
2026-03-19 09:05:51 +0100

    Jupyterhub metabolism installation fixes and improvements

--- cs/jupyterhub/ec2_instance.tf
+++ cs/jupyterhub/ec2_instance.tf
@@ -2,6 +2,53 @@ locals {
   ec2_name = basename(abspath(var.jupyterhub_base_path))
 }
 
+resource "aws_iam_role" "jupyterhub_ec2_role" {
+  name = "${local.ec2_name}_ec2_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+
+  tags = {
+    Name        = "${local.ec2_name}_ec2_role"
+    SBO_Billing = "jupyterhub_svc"
+  }
+}
+
+resource "aws_iam_role_policy" "jupyterhub_secrets_policy" {
+  name = "${local.ec2_name}_secrets_access"
+  role = aws_iam_role.jupyterhub_ec2_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["secretsmanager:GetSecretValue"]
+      Resource = [var.jupyterhub_secrets_arn]
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "jupyterhub_cloudwatch" {
+  role       = aws_iam_role.jupyterhub_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+resource "aws_iam_instance_profile" "jupyterhub_ec2_profile" {
+  name = "${local.ec2_name}_ec2_profile"
+  role = aws_iam_role.jupyterhub_ec2_role.name
+
+  tags = {
+    Name        = "${local.ec2_name}_ec2_profile"
+    SBO_Billing = "jupyterhub_svc"
+  }
+}
+
 data "aws_key_pair" "coreservices" {
   key_name           = var.aws_coreservices_ssh_key_id
   include_public_key = true
@@ -18,6 +65,7 @@ resource "aws_instance" "jupyterhub_server" {
   key_name                    = var.aws_coreservices_ssh_key_id
   vpc_security_group_ids      = [aws_security_group.jupyterhub_sg.id]
   associate_public_ip_address = false
+  iam_instance_profile        = aws_iam_instance_profile.jupyterhub_ec2_profile.name
 
   user_data_replace_on_change = true
   monitoring                  = true
@@ -45,7 +93,7 @@ resource "aws_instance" "jupyterhub_server" {
   }
 
   root_block_device {
-    volume_size = 20
+    volume_size = var.jupyterhub_ec2_root_volume_size
     encrypted   = true
   }
 
