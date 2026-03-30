3fe7fe8d522737ab9dcc538d37b94252b94c80e5
Daniel Fernández <daferoes@gmail.com>
2026-03-30 10:23:33 +0200

    Add nfs-utils to list of required packages

--- bastion_host/scripts/user_setup.sh
+++ bastion_host/scripts/user_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Install required packages
 dnf update -y
-dnf install -y git lsof nmap nmap-ncat postgresql17 rsync strace tcpdump tmux traceroute vim wget zsh openssh-server redis6
+dnf install -y git lsof nmap nmap-ncat postgresql17 rsync strace tcpdump tmux traceroute vim wget zsh openssh-server redis6 nfs-utils
 
 # Install SSM Agent for Amazon Linux 2023
 dnf install -y amazon-ssm-agent
