f5f6e3da8c1b7902e1a73b64d6a0e03c3ff3940a
Mike Gevaert <mgevaert@openbraininstitute.org>
2026-04-02 15:28:34 +0200

    typo

--- launch_system/compute_cell_definitions.tf
+++ launch_system/compute_cell_definitions.tf
@@ -47,7 +47,7 @@ locals {
           slurm_secret         = "$${SECRET:SLURM_SECRET}"
           instance_types = {
             small = [awscc_pcs_queue.pcs_queue_small.name]
-            large = concat([wscc_pcs_queue.pcs_queue_large.name],
+            large = concat([awscc_pcs_queue.pcs_queue_large.name],
             [for k, q in awscc_pcs_queue.pcs_queue_large_fallback : q.name])
           }
         }
