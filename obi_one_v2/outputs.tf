0b3cb151e4d66dfd982f6d024c29fc5990905de3
Daniel Fernández <daferoes@gmail.com>
2026-04-10 12:26:22 +0200

    Only allow a list of services to access accounting and launch-system
    accounting_svc: small_scale_simulator, obi_one, neuroagent, notebook_service, virtual_lab_manager, core_web_app (Azure)
    
    launch_system: obi_one, launch_system executors, pcs, bastion, aca (Azure), batch (Azure)

--- obi_one_v2/outputs.tf
+++ obi_one_v2/outputs.tf
@@ -2,3 +2,8 @@ output "private_lb_rule_suffix" {
   description = "Private Loadbalancer Rule Suffix"
   value       = aws_lb_target_group.private_obi_one_v2.arn_suffix
 }
+
+output "subnet_cidr_blocks" {
+  description = "CIDR blocks of the obi_one_v2 subnets"
+  value       = [aws_subnet.obi_one_v2.cidr_block]
+}
