6e567d9bf1741bbb194c4dac3553e7d02eaacdaa
Dries Verachtert <dries.verachtert@dries.eu>
2026-05-19 16:33:20 +0200

    Add dbload metric alerts, add account and auth-manager DB
    https://github.com/openbraininstitute/INFRA/issues/540

--- auth-manager/outputs.tf
+++ auth-manager/outputs.tf
@@ -2,3 +2,9 @@ output "private_lb_rule_suffix" {
   description = "auth manager Private Loadbalancer Rule Suffix"
   value       = aws_lb_target_group.auth_manager_private_tg.arn_suffix
 }
+
+output "rds_db_identifier" {
+  value       = aws_db_instance.auth_manager.identifier
+  description = "Identifier of the auth manager DB"
+  sensitive   = false
+}
