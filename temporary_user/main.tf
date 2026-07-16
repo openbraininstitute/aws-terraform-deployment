444c9b5659e6fb1ec8ec722e92f1f22d43e8c4fd
Erik Heeren <erik.heeren@epfl.ch>
2026-07-16 16:07:59 +0200

    Create backup user for NAS (#1602)

--- temporary_user/main.tf
+++ temporary_user/main.tf
@@ -7,15 +7,16 @@ resource "aws_iam_user_policy" "temp_user_policy" {
   user = aws_iam_user.temp_user.name
 
   policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = var.allow_actions
-        Resource = var.allow_resources
-      }
-    ]
+    Version   = "2012-10-17"
+    Statement = [for action in var.allow_resource_actions : { Effect = "Allow", Action = action["allow_actions"], Resource = action["resources"] }]
   })
+}
+
+resource "aws_iam_policy_attachment" "extra_policy" {
+  for_each   = toset(var.extra_policies)
+  name       = "extra policy"
+  policy_arn = each.key
+  users      = [aws_iam_user.temp_user.id]
 }
 
 resource "aws_iam_access_key" "temp_user_access_key" {
