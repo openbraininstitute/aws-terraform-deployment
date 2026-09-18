616b4d79787db6134349c3de57f1bc989e08ae31
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2026-09-18 20:33:28 +0200

    Quote ip_protocol = "-1" in the managed instances security group rules
    ip_protocol is a string attribute; the unquoted -1 was coerced. Matches the
    convention already used in s3files.tf. No change to the resulting state.

--- launch_system/executor_managed_instances.tf
+++ launch_system/executor_managed_instances.tf
@@ -90,7 +90,7 @@ resource "aws_security_group" "executor_cpu_instance" {
 
 resource "aws_vpc_security_group_egress_rule" "executor_cpu_instance_allow_outgoing" {
   security_group_id = aws_security_group.executor_cpu_instance.id
-  ip_protocol       = -1
+  ip_protocol       = "-1"
   cidr_ipv4         = "0.0.0.0/0"
   description       = "Allow all egress for ECS CPU instances"
 }
